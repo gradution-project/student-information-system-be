6629fb9477863b57213cbe645019af97ba114a96
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-10-31 20:53:22 +0300

    Backend Project of Student Information System Has Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/StudentInformationSystemApplication.java
@@ -0,0 +1,11 @@
+package com.graduationproject.studentinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInformationSystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StudentInformationSystemApplication.class, args);
+	}
+}
