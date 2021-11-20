9ce5722a0454a520d66fd0e60c8820c4e0e79610
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-20 21:10:15 +0300

    StudentStatus Field Has Been Added

--- src/main/java/com/graduationproject/studentinformationsystem/student/model/dto/response/StudentPersonalInfoResponseDto.java
+++ src/main/java/com/graduationproject/studentinformationsystem/student/model/dto/response/StudentPersonalInfoResponseDto.java
@@ -1,6 +1,7 @@
 package com.graduationproject.studentinformationsystem.student.model.dto.response;
 
 import com.graduationproject.studentinformationsystem.common.model.dto.response.BaseResponseDto;
+import com.graduationproject.studentinformationsystem.student.model.enums.StudentStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +17,7 @@ public class StudentPersonalInfoResponseDto extends BaseResponseDto {
     private String surname;
     private String email;
     private Long phoneNumber;
+    private StudentStatus status;
     private byte[] profilePhoto;
     private String profilePhotoUrl;
     private Date birthday;
--- src/main/java/com/graduationproject/studentinformationsystem/student/model/entity/StudentPersonalInfoEntity.java
+++ src/main/java/com/graduationproject/studentinformationsystem/student/model/entity/StudentPersonalInfoEntity.java
@@ -1,6 +1,7 @@
 package com.graduationproject.studentinformationsystem.student.model.entity;
 
 import com.graduationproject.studentinformationsystem.common.model.entity.BaseEntity;
+import com.graduationproject.studentinformationsystem.student.model.enums.StudentStatus;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
@@ -18,6 +19,7 @@ public class StudentPersonalInfoEntity extends BaseEntity {
     private String surname;
     private String email;
     private Long phoneNumber;
+    private StudentStatus status;
     private byte[] profilePhoto;
     private String profilePhotoUrl;
     private Date birthday;
