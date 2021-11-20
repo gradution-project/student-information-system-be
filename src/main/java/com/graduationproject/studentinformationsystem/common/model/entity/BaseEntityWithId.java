27e5e4ad13ced40c1a4247cf3cc2ca51407caef4
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-20 15:06:12 +0300

    Base Entity Classes Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/common/model/entity/BaseEntityWithId.java
@@ -0,0 +1,19 @@
+package com.graduationproject.studentinformationsystem.common.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@SuperBuilder
+public class BaseEntityWithId {
+
+    private Long id;
+    private Date createdDate;
+    private Long createdUserId;
+    private Date modifiedDate;
+    private Long modifiedUserId;
+}
