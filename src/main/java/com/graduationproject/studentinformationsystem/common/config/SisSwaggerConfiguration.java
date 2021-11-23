036df60686d5566c7c5cf9d980fc7e51937ab6f6
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-23 23:50:36 +0300

    Swagger Configurations Have Been Fixed and Base Error Controller Has Been Deleted from Swagger UI

--- src/main/java/com/graduationproject/studentinformationsystem/common/config/SisSwaggerConfiguration.java
+++ src/main/java/com/graduationproject/studentinformationsystem/common/config/SisSwaggerConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -14,15 +15,15 @@ public class SisSwaggerConfiguration {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select().paths(PathSelectors.regex("/.*"))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.graduationproject.studentinformationsystem"))
+                .paths(PathSelectors.regex("/.*"))
                 .build().apiInfo(apiEndPointsInfo());
     }
 
     private ApiInfo apiEndPointsInfo() {
         return new ApiInfoBuilder()
-                .title("Student Information System API Application")
-                .description("Student Information System API Documentation")
-                .license("Student Information System v1.0.0")
+                .title("Student Information System API")
                 .version("v1.0.0")
                 .build();
     }
