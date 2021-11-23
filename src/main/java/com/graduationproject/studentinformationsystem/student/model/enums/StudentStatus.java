2e9655d0f6df69f9ff3457ffac00307294241c64
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-23 23:41:43 +0300

    StudentStatus Enum Class Has Been Fixed and Updated

--- src/main/java/com/graduationproject/studentinformationsystem/student/model/enums/StudentStatus.java
+++ src/main/java/com/graduationproject/studentinformationsystem/student/model/enums/StudentStatus.java
@@ -1,16 +1,9 @@
 package com.graduationproject.studentinformationsystem.student.model.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum StudentStatus {
-
-    ACTIVE('A'),
-    PASSIVE('P'),
-    DELETED('D'),
-    GRADUATED('G');
-
-    private final Character character;
+    ACTIVE,
+    PASSIVE,
+    DELETED,
+    GRADUATED,
+    ALL
 }
