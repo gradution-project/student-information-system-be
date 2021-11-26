19e196971a6b205a37b347be668984e493bc1533
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-26 13:45:03 +0300

    SisControllerEndpoint Class Has Been Created & Student Endpoint Has Been Added

---
+++ src/main/java/com/graduationproject/studentinformationsystem/common/util/controller/endpoint/SisControllerEndpoint.java
@@ -0,0 +1,14 @@
+package com.graduationproject.studentinformationsystem.common.util.controller.endpoint;
+
+public class SisControllerEndpoint {
+
+    private SisControllerEndpoint() {
+    }
+
+    public static class Path {
+        private Path() {
+        }
+
+        public static final String STUDENT = "/student";
+    }
+}
