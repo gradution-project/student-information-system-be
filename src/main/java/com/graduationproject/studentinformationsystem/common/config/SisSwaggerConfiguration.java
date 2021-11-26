5f9b754f6d41c1927ef94cdfe73e5056164d0141
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-26 13:45:25 +0300

    STUDENT API TAG Has Been Added

--- src/main/java/com/graduationproject/studentinformationsystem/common/config/SisSwaggerConfiguration.java
+++ src/main/java/com/graduationproject/studentinformationsystem/common/config/SisSwaggerConfiguration.java
@@ -6,11 +6,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SisSwaggerConfiguration {
+
+    public static final String STUDENT_API_TAG = "Student Controller";
 
     @Bean
     public Docket api() {
@@ -18,7 +21,10 @@ public class SisSwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.graduationproject.studentinformationsystem"))
                 .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiEndPointsInfo());
+                .build().apiInfo(apiEndPointsInfo())
+                .tags(
+                        new Tag(STUDENT_API_TAG, "STUDENT_ACADEMIC_INFO & STUDENT_PERSONAL_INFO")
+                );
     }
 
     private ApiInfo apiEndPointsInfo() {
