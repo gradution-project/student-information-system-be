06fd71ddc7456b11f90482f1bac19cbf7e4b37b6
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-26 13:15:55 +0300

    SisBaseResponseUtil Methods Have Been Fixed

--- src/main/java/com/graduationproject/studentinformationsystem/common/util/response/SisBaseResponseUtil.java
+++ src/main/java/com/graduationproject/studentinformationsystem/common/util/response/SisBaseResponseUtil.java
@@ -10,11 +10,6 @@ public class SisBaseResponseUtil {
 
     public static final String SUCCESS = "Success";
 
-    private static final String DEFAULT_FOUND_MESSAGE = "Found Successfully";
-    private static final String DEFAULT_SAVED_MESSAGE = "Saved Successfully";
-    private static final String DEFAULT_UPDATED_MESSAGE = "Updated Successfully";
-    private static final String DEFAULT_DELETED_MESSAGE = "Deleted Successfully";
-
     public static <T> ResponseEntity<SisBaseResponse<T>> generateSisResponse(HttpStatus status, String message, T response) {
         SisBaseResponse<T> sisResponse = new SisBaseResponse<>();
         sisResponse.setHttpStatus(status);
@@ -24,23 +19,7 @@ public class SisBaseResponseUtil {
         return new ResponseEntity<>(sisResponse, status);
     }
 
-    public static <T> ResponseEntity<SisBaseResponse<T>> successResponseFound(T result) {
-        return successResponse(result, DEFAULT_FOUND_MESSAGE);
-    }
-
-    public static <T> ResponseEntity<SisBaseResponse<T>> successResponseSaved(T result) {
-        return successResponse(result, DEFAULT_SAVED_MESSAGE);
-    }
-
-    public static <T> ResponseEntity<SisBaseResponse<T>> successResponseUpdated(T result) {
-        return successResponse(result, DEFAULT_UPDATED_MESSAGE);
-    }
-
-    public static <T> ResponseEntity<SisBaseResponse<T>> successResponseDeleted(T result) {
-        return successResponse(result, DEFAULT_DELETED_MESSAGE);
-    }
-
-    private static <T> ResponseEntity<SisBaseResponse<T>> successResponse(T response, String message) {
-        return generateSisResponse(HttpStatus.OK, message, response);
+    public static <T> ResponseEntity<SisBaseResponse<T>> successResponse(T response) {
+        return generateSisResponse(HttpStatus.OK, SUCCESS, response);
     }
 }
