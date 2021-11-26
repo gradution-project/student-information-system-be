45362e61dfff0aaecdb7d3e25b374f71e4c26d04
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-26 13:43:27 +0300

    Student Service Classes and Methods Have Been Added for Merge to StudentAcademicInfoService and StudentPersonalInfoService

---
+++ src/main/java/com/graduationproject/studentinformationsystem/student/service/StudentService.java
@@ -0,0 +1,35 @@
+package com.graduationproject.studentinformationsystem.student.service;
+
+import com.graduationproject.studentinformationsystem.student.model.dto.request.StudentAcademicInfoRequest;
+import com.graduationproject.studentinformationsystem.student.model.dto.request.StudentInfoRequest;
+import com.graduationproject.studentinformationsystem.student.model.dto.request.StudentPersonalInfoRequest;
+import com.graduationproject.studentinformationsystem.student.model.dto.response.StudentAcademicInfoResponse;
+import com.graduationproject.studentinformationsystem.student.model.dto.response.StudentInfoDetailResponse;
+import com.graduationproject.studentinformationsystem.student.model.dto.response.StudentPersonalInfoResponse;
+import com.graduationproject.studentinformationsystem.student.model.dto.response.StudentResponse;
+import com.graduationproject.studentinformationsystem.student.model.enums.StudentStatus;
+
+import java.util.List;
+
+public interface StudentService {
+
+    List<StudentResponse> getAllStudentsByStatus(StudentStatus status);
+
+    StudentInfoDetailResponse getStudentDetailById(Long studentId);
+
+    StudentInfoDetailResponse saveStudent(StudentInfoRequest studentInfoRequest);
+
+    StudentAcademicInfoResponse updateStudentAcademicInfo(Long studentId,
+                                                          StudentAcademicInfoRequest academicInfoRequest);
+
+    StudentPersonalInfoResponse updateStudentPersonalInfo(Long studentId,
+                                                          StudentPersonalInfoRequest personalInfoRequest);
+
+    StudentResponse deleteStudent(Long studentId);
+
+    StudentResponse passivateStudent(Long studentId);
+
+    StudentResponse activateStudent(Long studentId);
+
+    StudentResponse graduateStudent(Long studentId);
+}
