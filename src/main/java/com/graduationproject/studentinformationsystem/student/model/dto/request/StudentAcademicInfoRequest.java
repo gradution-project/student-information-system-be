6e79321db3105d674ccbdca69b1a7316eda0cad0
agitrubard <demiragitrubar@gmail.com>
2021-11-26 13:23:39 +0300

    StudentAcademicInfoRequest Has Been Fixed

--- src/main/java/com/graduationproject/studentinformationsystem/student/model/dto/request/StudentAcademicInfoRequest.java
+++ src/main/java/com/graduationproject/studentinformationsystem/student/model/dto/request/StudentAcademicInfoRequest.java
@@ -1,13 +1,12 @@
 package com.graduationproject.studentinformationsystem.student.model.dto.request;
 
+import com.graduationproject.studentinformationsystem.common.util.group.OnCreate;
 import com.graduationproject.studentinformationsystem.common.util.validation.OnlyNumber;
 import com.graduationproject.studentinformationsystem.student.model.enums.StudentClassLevel;
 import com.graduationproject.studentinformationsystem.student.model.enums.StudentDegree;
-import com.graduationproject.studentinformationsystem.student.model.enums.StudentStatus;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -15,22 +14,14 @@ import javax.validation.constraints.Size;
 @Setter
 public class StudentAcademicInfoRequest {
 
-    @NotNull
     @OnlyNumber
-    @Size(max = 3)
+    @Size(max = 5)
+    @NotNull(groups = {OnCreate.class})
     private Long departmentId;
 
-    @NotNull
+    @NotNull(groups = {OnCreate.class})
     private StudentDegree degree;
 
-    @NotNull
+    @NotNull(groups = {OnCreate.class})
     private StudentClassLevel classLevel;
-
-    @NotNull
-    @Email
-    @Size(max = 100)
-    private String email;
-
-    @NotNull
-    private StudentStatus status;
 }
