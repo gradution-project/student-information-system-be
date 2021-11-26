4f56081c2cbd5d33ef379bb764d276b15e81843c
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-26 13:11:28 +0300

    getCurrentYear Method Has Been Added to SisUtil Class

--- src/main/java/com/graduationproject/studentinformationsystem/common/util/SisUtil.java
+++ src/main/java/com/graduationproject/studentinformationsystem/common/util/SisUtil.java
@@ -1,7 +1,17 @@
 package com.graduationproject.studentinformationsystem.common.util;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class SisUtil {
 
     private SisUtil() {
+    }
+
+    public static String getCurrentYear() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        Integer year = calendar.get(Calendar.YEAR);
+        return String.valueOf(year);
     }
 }
