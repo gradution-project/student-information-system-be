f33c05187f80566c8bcec876af682954554dd5e5
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-29 22:18:23 +0300

    Teacher Groups Have Been Added

--- src/main/java/com/graduationproject/studentinformationsystem/teacher/model/enums/TeacherGroup.java
+++ src/main/java/com/graduationproject/studentinformationsystem/teacher/model/enums/TeacherGroup.java
@@ -1,5 +1,9 @@
 package com.graduationproject.studentinformationsystem.teacher.model.enums;
 
 public enum TeacherGroup {
-    // TODO: Teacher Groups
+    RESEARCH_ASSOCIATE,
+    TEACHING_ASSOCIATE,
+    ASSISTANT_PROFESSOR,
+    ASSOCIATE_PROFESSOR,
+    PROFESSOR
 }
