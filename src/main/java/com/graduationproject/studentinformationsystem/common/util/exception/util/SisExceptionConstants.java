85c960f9084e3853306b1c51a62147cb2aa9cf33
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-12-07 22:51:23 +0300

    Util Classes Have Been Created for Exceptions

---
+++ src/main/java/com/graduationproject/studentinformationsystem/common/util/exception/util/SisExceptionConstants.java
@@ -0,0 +1,12 @@
+package com.graduationproject.studentinformationsystem.common.util.exception.util;
+
+public class SisExceptionConstants {
+
+    private SisExceptionConstants() {
+    }
+
+    public static final String API_ERROR = "API ERROR";
+    public static final String NOT_FOUND = "NOT FOUND";
+    public static final String VALIDATION_ERROR = "VALIDATION ERROR";
+    public static final String DATABASE_ERROR = "DATABASE ERROR";
+}
