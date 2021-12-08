7468c71423aa09b8db99ef32c364d514679f8cd8
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-12-08 14:42:06 +0300

    TeacherPersonalInfoResponse Has Been Fixed with SisBasePersonalInfoResponse

--- src/main/java/com/graduationproject/studentinformationsystem/teacher/model/dto/response/TeacherPersonalInfoResponse.java
+++ src/main/java/com/graduationproject/studentinformationsystem/teacher/model/dto/response/TeacherPersonalInfoResponse.java
@@ -1,25 +1,10 @@
 package com.graduationproject.studentinformationsystem.teacher.model.dto.response;
 
-import com.graduationproject.studentinformationsystem.common.model.dto.response.SisBaseResponse;
-import com.graduationproject.studentinformationsystem.teacher.model.enums.TeacherStatus;
+import com.graduationproject.studentinformationsystem.common.util.controller.response.SisBasePersonalInfoResponse;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
-
 @Getter
 @Setter
-public class TeacherPersonalInfoResponse extends SisBaseResponse {
-
-    private Long teacherId;
-    private Long tcNo;
-    private String name;
-    private String surname;
-    private String email;
-    private String phoneNumber;
-    private TeacherStatus status;
-    //    private Byte[] profilePhoto; // TODO: Profile Photo
-//    private String profilePhotoUrl; // TODO: Profile Photo URL
-    private Date birthday;
-    private String address;
+public class TeacherPersonalInfoResponse extends SisBasePersonalInfoResponse {
 }
