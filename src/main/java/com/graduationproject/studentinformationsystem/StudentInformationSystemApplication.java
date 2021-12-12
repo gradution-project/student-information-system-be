969cb9c8c62c8fcfc77d9eb4c78cc9cabec6ef48
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-12-12 13:37:44 +0300

    PasswordEncoder Has Been Created

--- src/main/java/com/graduationproject/studentinformationsystem/StudentInformationSystemApplication.java
+++ src/main/java/com/graduationproject/studentinformationsystem/StudentInformationSystemApplication.java
@@ -5,10 +5,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Slf4j
 @SpringBootApplication
 public class StudentInformationSystemApplication implements ApplicationListener<ApplicationReadyEvent> {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentInformationSystemApplication.class, args);
 	}
