db42c3275fda6ccc05f2c297a202c12c83ae9fa6
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-12-14 21:11:57 +0300

    Heroku Master Deployment Fixes

---
+++ src/main/java/com/graduationproject/studentinformationsystem/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.graduationproject.studentinformationsystem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StudentInformationSystemApplication.class);
+    }
+
+}
