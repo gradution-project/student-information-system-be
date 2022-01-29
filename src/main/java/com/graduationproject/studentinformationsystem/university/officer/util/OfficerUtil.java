f4b1c703462d14c77c643f0f5ec606f5fad069bc
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-01-29 14:28:16 +0300

    Officer Util Class Has Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/officer/util/OfficerUtil.java
@@ -0,0 +1,32 @@
+package com.graduationproject.studentinformationsystem.university.officer.util;
+
+import com.graduationproject.studentinformationsystem.common.util.SisUtil;
+
+import java.util.List;
+
+public class OfficerUtil {
+
+    private OfficerUtil() {
+    }
+
+    /**
+     * Officer Email : name.surname@university.edu.tr
+     */
+    public static String generateOfficerEmail(String name, String surname) {
+        name = SisUtil.stringToStringLowerCaseWithDot(name);
+        surname = SisUtil.stringToStringLowerCaseWithDot(surname);
+        return name + "." + surname + "@university.edu.tr";
+    }
+
+    /**
+     * Officer ID : facultyId + officerNumber
+     */
+    public static Long generateOfficerId(final Long facultyId, final List<Long> officerIds) {
+        while (true) {
+            final Long officerId = SisUtil.generateRandomIdWithPrefixId(facultyId);
+            if (SisUtil.isNotExistIdInIdList(officerId, officerIds)) {
+                return officerId;
+            }
+        }
+    }
+}
