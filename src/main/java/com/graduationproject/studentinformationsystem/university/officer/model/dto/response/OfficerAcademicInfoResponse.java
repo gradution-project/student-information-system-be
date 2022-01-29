0ecb31dc6eccc55166d97d3f2073c3ddb5bc9c7a
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-01-29 14:30:14 +0300

    Officer Response Classes Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/officer/model/dto/response/OfficerAcademicInfoResponse.java
@@ -0,0 +1,13 @@
+package com.graduationproject.studentinformationsystem.university.officer.model.dto.response;
+
+import com.graduationproject.studentinformationsystem.common.util.controller.response.SisBaseAcademicInfoResponse;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class OfficerAcademicInfoResponse extends SisBaseAcademicInfoResponse {
+
+    private Long officerId;
+    private Long facultyId;
+}
