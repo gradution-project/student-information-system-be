31ba4b6345ae2c970cce12573b968774e787bbe3
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-01-29 16:05:55 +0300

    Officer Login API Has Been Activated and Fixes

--- src/main/java/com/graduationproject/studentinformationsystem/login/officer/service/impl/OfficerLoginServiceImpl.java
+++ src/main/java/com/graduationproject/studentinformationsystem/login/officer/service/impl/OfficerLoginServiceImpl.java
@@ -3,92 +3,98 @@ package com.graduationproject.studentinformationsystem.login.officer.service.imp
 import com.graduationproject.studentinformationsystem.common.util.exception.SisNotExistException;
 import com.graduationproject.studentinformationsystem.login.common.model.response.ForgotPasswordResponse;
 import com.graduationproject.studentinformationsystem.login.common.model.response.LoginResponse;
+import com.graduationproject.studentinformationsystem.login.common.service.PasswordService;
 import com.graduationproject.studentinformationsystem.login.officer.model.dto.request.OfficerForgotPasswordRequest;
 import com.graduationproject.studentinformationsystem.login.officer.model.dto.request.OfficerLoginRequest;
+import com.graduationproject.studentinformationsystem.login.officer.model.entity.OfficerLoginInfoEntity;
+import com.graduationproject.studentinformationsystem.login.officer.repository.OfficerLoginRepository;
 import com.graduationproject.studentinformationsystem.login.officer.service.OfficerLoginService;
+import com.graduationproject.studentinformationsystem.university.mail.service.OfficerMailService;
+import com.graduationproject.studentinformationsystem.university.officer.model.dto.response.OfficerInfoDetailResponse;
+import com.graduationproject.studentinformationsystem.university.officer.service.OfficerAcademicInfoService;
+import com.graduationproject.studentinformationsystem.university.officer.service.OfficerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-//TODO: Officer Login Service Must Be Add
+import java.util.Date;
+
 @Service
 @RequiredArgsConstructor
 public class OfficerLoginServiceImpl implements OfficerLoginService {
 
+    private final OfficerService studentService;
+    private final OfficerAcademicInfoService academicInfoService;
+    private final OfficerMailService mailService;
+
+    private final OfficerLoginRepository loginRepository;
+    private final PasswordService passwordService;
+
     @Override
     public LoginResponse login(final OfficerLoginRequest loginRequest) {
-        return null;
+        final Long officerId = loginRequest.getOfficerId();
+
+        if (isOfficerActive(officerId)) {
+
+            if (checkPassword(loginRequest)) {
+                loginRepository.updateLoginInfo(generateSuccessLoginEntity(officerId));
+                return getSuccessLoginResponse();
+            }
+
+            if (checkFailCounter(officerId)) {
+                return getFailLoginResponse();
+            }
+        }
+
+        return getFailLoginResponse();
     }
 
     @Override
     public ForgotPasswordResponse forgotPassword(final OfficerForgotPasswordRequest forgotPasswordRequest) throws SisNotExistException {
-        return null;
+        final OfficerInfoDetailResponse infoDetailResponse = studentService.getOfficerDetailById(forgotPasswordRequest.getOfficerId());
+
+        mailService.sendForgotPasswordEmail(infoDetailResponse);
+
+        return ForgotPasswordResponse.builder()
+                .isForgotPasswordSuccess(true).build();
     }
 
-//    private final OfficerService studentService;
-//    private final OfficerAcademicInfoService academicInfoService;
-//    private final OfficerMailService mailService;
-//
-//    private final OfficerLoginRepository loginRepository;
-//    private final PasswordService passwordService;
-//
-//    @Override
-//    public LoginResponse login(OfficerLoginRequest loginRequest) {
-//        final Long officerId = loginRequest.getofficerId();
-//
-//        if (isOfficerActive(officerId)) {
-//
-//            if (checkPassword(loginRequest)) {
-//                loginRepository.updateLoginInfo(generateSuccessLoginEntity(officerId));
-//                return getSuccessLoginResponse();
-//            }
-//
-//            if (checkFailCounter(officerId)) {
-//                return getFailLoginResponse();
-//            }
-//        }
-//
-//        return getFailLoginResponse();
-//    }
-//
-//    @Override
-//    public ForgotPasswordResponse forgotPassword(OfficerForgotPasswordRequest forgotPasswordRequest) throws SisNotExistException {
-//        mailService.sendForgotPasswordEmail(studentService.getOfficerDetailById(forgotPasswordRequest.getofficerId()));
-//        return ForgotPasswordResponse.builder().isForgotPasswordSuccess(true).build();
-//    }
-//
-//
-//    private boolean isOfficerActive(Long officerId) {
-//        return academicInfoService.isOfficerActive(officerId);
-//    }
-//
-//    protected boolean checkPassword(OfficerLoginRequest loginRequest) {
-//        return passwordService.isPasswordTrue(loginRequest.getPassword(), loginRepository.getPassword(loginRequest.getofficerId()));
-//    }
-//
-//    private boolean checkFailCounter(Long officerId) {
-//        final Integer failCounter = loginRepository.getFailCounter(officerId);
-//        if (failCounter < 3) {
-//            loginRepository.updateFailCounter(officerId);
-//            return true;
-//        }
-//        // TODO: Change Password
-//        return true;
-//    }
-//
-//    private LoginResponse getSuccessLoginResponse() {
-//        return LoginResponse.builder().isLoginSuccess(true).build();
-//    }
-//
-//    private LoginResponse getFailLoginResponse() {
-//        return LoginResponse.builder().isLoginSuccess(false).build();
-//    }
-//
-//    private OfficerLoginInfoEntity generateSuccessLoginEntity(Long officerId) {
-//
-//        return OfficerLoginInfoEntity.builder()
-//                .officerId(officerId)
-//                .failCounter(0)
-//                .lastLoginDate(new Date())
-//                .build();
-//    }
+
+    private boolean isOfficerActive(final Long officerId) {
+        return academicInfoService.isOfficerActive(officerId);
+    }
+
+    protected boolean checkPassword(final OfficerLoginRequest loginRequest) {
+        final String password = loginRequest.getPassword();
+        final String passwordFromDb = loginRepository.getPassword(loginRequest.getOfficerId());
+
+        return passwordService.isPasswordTrue(password, passwordFromDb);
+    }
+
+    private boolean checkFailCounter(final Long officerId) {
+        final Integer failCounter = loginRepository.getFailCounter(officerId);
+        if (failCounter < 3) {
+            loginRepository.updateFailCounter(officerId);
+            return true;
+        }
+        // TODO: Change Password
+        return true;
+    }
+
+    private LoginResponse getSuccessLoginResponse() {
+        return LoginResponse.builder()
+                .isLoginSuccess(true).build();
+    }
+
+    private LoginResponse getFailLoginResponse() {
+        return LoginResponse.builder()
+                .isLoginSuccess(false).build();
+    }
+
+    private OfficerLoginInfoEntity generateSuccessLoginEntity(final Long officerId) {
+        return OfficerLoginInfoEntity.builder()
+                .officerId(officerId)
+                .failCounter(0)
+                .lastLoginDate(new Date())
+                .build();
+    }
 }
