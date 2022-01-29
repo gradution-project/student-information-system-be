aa92465d1217dc5948468aa20976ca7931fbfba2
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-01-29 14:40:02 +0300

    Officer Mail Service Classes Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/mail/service/OfficerMailService.java
@@ -0,0 +1,10 @@
+package com.graduationproject.studentinformationsystem.university.mail.service;
+
+import com.graduationproject.studentinformationsystem.university.officer.model.dto.response.OfficerInfoDetailResponse;
+
+public interface OfficerMailService {
+
+    void sendFirstPasswordEmail(final OfficerInfoDetailResponse infoDetailResponse);
+
+    void sendForgotPasswordEmail(final OfficerInfoDetailResponse infoDetailResponse);
+}
