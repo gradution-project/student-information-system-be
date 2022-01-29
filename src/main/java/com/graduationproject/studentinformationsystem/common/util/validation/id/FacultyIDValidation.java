df3ce47f59d88cc148ec75bc2e2420f1bbc6ba12
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-01-29 14:35:49 +0300

    FacultyID Validation Classes Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/common/util/validation/id/FacultyIDValidation.java
@@ -0,0 +1,22 @@
+package com.graduationproject.studentinformationsystem.common.util.validation.id;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Objects;
+
+public class FacultyIDValidation implements ConstraintValidator<FacultyID, Object> {
+
+    public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+        if (value == null) return true;
+
+        final int length = 2;
+
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate("length must be " + length)
+                .addConstraintViolation();
+
+        return NumberUtils.isCreatable(value.toString()) && Objects.equals(length, value.toString().length());
+    }
+}
