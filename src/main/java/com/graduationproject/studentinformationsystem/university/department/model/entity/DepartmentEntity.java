bb17ea55346639f9d5f2c5da98f03f87ced93e21
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-01-31 15:33:41 +0300

    FacultyResponse Have Been Added to Department API Response and Refactored

--- src/main/java/com/graduationproject/studentinformationsystem/university/department/model/entity/DepartmentEntity.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/department/model/entity/DepartmentEntity.java
@@ -2,7 +2,9 @@ package com.graduationproject.studentinformationsystem.university.department.mod
 
 import com.graduationproject.studentinformationsystem.common.model.entity.SisBaseEntity;
 import com.graduationproject.studentinformationsystem.university.department.model.enums.DepartmentStatus;
+import com.graduationproject.studentinformationsystem.university.faculty.model.entity.FacultyEntity;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
@@ -15,4 +17,7 @@ public class DepartmentEntity extends SisBaseEntity {
     private DepartmentStatus status;
     private Integer totalClassLevel;
     private Boolean isTherePreparatoryClass;
+
+    @Setter
+    private FacultyEntity facultyEntity;
 }
--- src/main/java/com/graduationproject/studentinformationsystem/university/department/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/department/service/impl/DepartmentServiceImpl.java
@@ -11,6 +11,8 @@ import com.graduationproject.studentinformationsystem.university.department.mode
 import com.graduationproject.studentinformationsystem.university.department.repository.DepartmentRepository;
 import com.graduationproject.studentinformationsystem.university.department.service.DepartmentService;
 import com.graduationproject.studentinformationsystem.university.department.util.DepartmentUtil;
+import com.graduationproject.studentinformationsystem.university.faculty.model.entity.FacultyEntity;
+import com.graduationproject.studentinformationsystem.university.faculty.model.exception.FacultyException;
 import com.graduationproject.studentinformationsystem.university.faculty.repository.FacultyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,8 +28,9 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public List<DepartmentResponse> getAllDepartmentsByStatus(final DepartmentStatus status) {
-        final List<DepartmentEntity> departmentResponses = departmentRepository.getAllDepartmentsByStatus(status);
-        return DepartmentInfoConverter.entitiesToResponses(departmentResponses);
+        final List<DepartmentEntity> departmentEntities = departmentRepository.getAllDepartmentsByStatus(status);
+        setFacultyEntities(departmentEntities);
+        return DepartmentInfoConverter.entitiesToResponses(departmentEntities);
     }
 
     @Override
@@ -55,7 +58,8 @@ public class DepartmentServiceImpl implements DepartmentService {
     public DepartmentResponse updateDepartment(final Long departmentId, final DepartmentUpdateRequest updateRequest)
             throws SisNotExistException {
 
-        checkBeforeUpdating(departmentId);
+        final Long facultyId = updateRequest.getDepartmentInfoRequest().getFacultyId();
+        checkBeforeUpdating(departmentId, facultyId);
 
         final DepartmentEntity departmentEntity = DepartmentInfoConverter.generateUpdateEntity(departmentId, updateRequest);
         departmentRepository.updateDepartment(departmentEntity);
@@ -107,7 +111,18 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     private DepartmentResponse getDepartmentResponse(final Long departmentId) {
         final DepartmentEntity departmentEntity = departmentRepository.getDepartmentById(departmentId);
+        setFacultyEntity(departmentEntity);
         return DepartmentInfoConverter.entityToResponse(departmentEntity);
+    }
+
+    private void setFacultyEntity(final DepartmentEntity departmentEntity) {
+        final Long facultyId = departmentEntity.getFacultyId();
+        final FacultyEntity facultyEntity = facultyRepository.getFacultyById(facultyId);
+        departmentEntity.setFacultyEntity(facultyEntity);
+    }
+
+    private void setFacultyEntities(final List<DepartmentEntity> departmentEntities) {
+        departmentEntities.forEach(this::setFacultyEntity);
     }
 
 
@@ -120,9 +135,10 @@ public class DepartmentServiceImpl implements DepartmentService {
         ifFacultyIsNotExistThrowNotExistException(facultyId);
     }
 
-    private void checkBeforeUpdating(final Long departmentId) throws SisNotExistException {
+    private void checkBeforeUpdating(final Long departmentId, final Long facultyId) throws SisNotExistException {
 
         ifDepartmentIsNotExistThrowNotExistException(departmentId);
+        ifFacultyIsNotExistThrowNotExistException(facultyId);
     }
 
     private void checkBeforeDeleting(final Long departmentId) throws SisNotExistException, SisAlreadyException {
@@ -149,7 +165,7 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     private void ifFacultyIsNotExistThrowNotExistException(final Long facultyId) throws SisNotExistException {
         if (!facultyRepository.isFacultyExist(facultyId)) {
-            DepartmentException.throwNotExistException(facultyId);
+            FacultyException.throwNotExistException(facultyId);
         }
     }
 
