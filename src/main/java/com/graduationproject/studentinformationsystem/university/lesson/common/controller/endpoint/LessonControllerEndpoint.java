5179e4f777de618a7bcc7c36aeb89fdb77ba7346
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-01-31 14:12:30 +0300

    Lesson Controller Classes Have Been Created

--- src/main/java/com/graduationproject/studentinformationsystem/university/lesson/common/controller/endpoint/LessonControllerEndpoint.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/lesson/common/controller/endpoint/LessonControllerEndpoint.java
@@ -5,6 +5,11 @@ public class LessonControllerEndpoint {
     private LessonControllerEndpoint() {
     }
 
-    public static final String TEACHER = "/teacher";
-    public static final String TEACHER_BY_ID = TEACHER + "/{teacherId}";
+    public static final String LESSON_ID = "/{lessonId}";
+    public static final String SAVE = "/save";
+    public static final String UPDATE = "/update";
+    public static final String DELETE = "/delete";
+    public static final String PASSIVATE = "/passivate";
+    public static final String ACTIVATE = "/activate";
+    public static final String UPDATE_BY_LESSON_ID = UPDATE + LESSON_ID;
 }
