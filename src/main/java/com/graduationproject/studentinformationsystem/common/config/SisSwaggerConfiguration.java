0bdc1f61f125c45aa7171f9926b1c6456aa650cc
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-01-31 14:15:40 +0300

    SisSwaggerConfiguration Class Has Been Refactored and Updated

--- src/main/java/com/graduationproject/studentinformationsystem/common/config/SisSwaggerConfiguration.java
+++ src/main/java/com/graduationproject/studentinformationsystem/common/config/SisSwaggerConfiguration.java
@@ -13,12 +13,14 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SisSwaggerConfiguration {
 
-    public static final String LOGIN_API_TAG = "Login Controller";
+    public static final String FACULTY_API_TAG = "Faculty Controller";
+    public static final String DEPARTMENT_API_TAG = "Department Controller";
+    public static final String LESSON_API_TAG = "Lesson Controller";
     public static final String STUDENT_API_TAG = "Student Controller";
     public static final String TEACHER_API_TAG = "Teacher Controller";
+    public static final String TEACHER_LESSON_API_TAG = "Teacher Lesson Controller";
     public static final String OFFICER_API_TAG = "Officer Controller";
-    public static final String LESSON_API_TAG = "Lesson Controller";
-    public static final String FACULTY_API_TAG = "Faculty Controller";
+    public static final String LOGIN_API_TAG = "Login Controller";
 
     @Bean
     public Docket api() {
@@ -28,12 +30,13 @@ public class SisSwaggerConfiguration {
                 .paths(PathSelectors.regex("/.*"))
                 .build().apiInfo(apiEndPointsInfo())
                 .tags(
-                        new Tag(LOGIN_API_TAG, "AUTH_STUDENT_LOGIN & AUTH_TEACHER_LOGIN & AUTH_OFFICER_LOGIN"),
+                        new Tag(FACULTY_API_TAG, "UNIV_FACULTY"),
+                        new Tag(DEPARTMENT_API_TAG, "UNIV_DEPARTMENT"),
+                        new Tag(LESSON_API_TAG, "UNIV_LESSON & TEACHER_LESSON"),
                         new Tag(STUDENT_API_TAG, "STUDENT_ACADEMIC_INFO & STUDENT_PERSONAL_INFO"),
                         new Tag(TEACHER_API_TAG, "TEACHER_ACADEMIC_INFO & TEACHER_PERSONAL_INFO"),
                         new Tag(OFFICER_API_TAG, "OFFICER_ACADEMIC_INFO & OFFICER_PERSONAL_INFO"),
-                        new Tag(LESSON_API_TAG, "UNIV_LESSON & TEACHER_LESSON"),
-                        new Tag(FACULTY_API_TAG, "UNIV_FACULTY")
+                        new Tag(LOGIN_API_TAG, "AUTH_STUDENT_LOGIN & AUTH_TEACHER_LOGIN & AUTH_OFFICER_LOGIN")
                 );
     }
 
