8f94c443b00fabddf54bcaefe524bb79d505d710
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-01-31 14:15:09 +0300

    Teacher Lesson Controller Classes Have Been Refactored and Updated

---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/lesson/teacher/controller/endpoint/TeacherLessonControllerEndpoint.java
@@ -0,0 +1,11 @@
+package com.graduationproject.studentinformationsystem.university.lesson.teacher.controller.endpoint;
+
+public class TeacherLessonControllerEndpoint {
+
+    private TeacherLessonControllerEndpoint() {
+    }
+
+    public static final String TEACHER_ID = "/{teacherId}";
+    public static final String SAVE = "/save";
+    public static final String DELETE = "/delete";
+}
