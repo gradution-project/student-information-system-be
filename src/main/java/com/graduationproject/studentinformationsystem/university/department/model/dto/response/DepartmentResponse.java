00a588ee02f1397d73c60a7caf4fd91cafc8ee7f
Agit Rubar Demir <61833677+agitrubard@users.noreply.github.com>
2022-02-12 16:52:43 +0300

    API Enum Responses Fixes | SIS-99 (#50)
    * All API Enum Responses Have Been Fixed and Updated
    
    * SisBaseAcademicInfoResponse and SisBasePersonalInfoResponse Classes Have Been Moved to common/model Package
    
    * DepartmentStatus Enum Class and Responses Have Been Fixed and Updated

--- src/main/java/com/graduationproject/studentinformationsystem/university/department/model/dto/response/DepartmentResponse.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/department/model/dto/response/DepartmentResponse.java
@@ -1,6 +1,7 @@
 package com.graduationproject.studentinformationsystem.university.department.model.dto.response;
 
 import com.graduationproject.studentinformationsystem.common.model.dto.response.SisBaseResponse;
+import com.graduationproject.studentinformationsystem.university.department.model.enums.DepartmentStatus;
 import com.graduationproject.studentinformationsystem.university.faculty.model.dto.response.FacultyResponse;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
@@ -11,7 +12,7 @@ public class DepartmentResponse extends SisBaseResponse {
 
     private Long departmentId;
     private String name;
-    private String status;
+    private DepartmentStatus status;
     private Integer totalClassLevel;
     private Boolean isTherePreparatoryClass;
 
