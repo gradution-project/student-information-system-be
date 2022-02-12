b81c8ef976160ffc60b690887095f58b656eafa4
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-12 13:11:50 +0300

    ExamScheduleFileException Class Has Been Fixed and Updated

--- src/main/java/com/graduationproject/studentinformationsystem/university/schedule/exam/model/exception/ExamScheduleFileException.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/schedule/exam/model/exception/ExamScheduleFileException.java
@@ -2,6 +2,7 @@ package com.graduationproject.studentinformationsystem.university.schedule.exam.
 
 import com.graduationproject.studentinformationsystem.common.util.exception.SisAlreadyException;
 import com.graduationproject.studentinformationsystem.common.util.exception.SisException;
+import com.graduationproject.studentinformationsystem.common.util.exception.SisFileTypeException;
 import com.graduationproject.studentinformationsystem.common.util.exception.SisNotExistException;
 
 public class ExamScheduleFileException extends SisException {
@@ -9,19 +10,19 @@ public class ExamScheduleFileException extends SisException {
     private ExamScheduleFileException() {
     }
 
-    public static void throwNotExistException(final Long fileId) throws SisNotExistException {
-        throwNotExistException("EXAM SCHEDULE FILE IS NOT EXIST! fileId:" + fileId);
+    public static void throwNotExistException(final Long departmentId) throws SisNotExistException {
+        SisException.throwNotExistException("EXAM SCHEDULE FILE IS NOT EXIST! departmentId:" + departmentId);
     }
 
-    public static void throwAlreadyActiveException(final Long fileId) throws SisAlreadyException {
-        throwAlreadyException("EXAM SCHEDULE FILE IS ALREADY ACTIVE! fileId:" + fileId);
+    public static void throwNotExistException(final String fileId) throws SisNotExistException {
+        SisException.throwNotExistException("EXAM SCHEDULE FILE IS NOT EXIST! fileId:" + fileId);
     }
 
-    public static void throwAlreadyPassiveException(final Long fileId) throws SisAlreadyException {
-        throwAlreadyException("EXAM SCHEDULE FILE IS ALREADY PASSIVE! fileId:" + fileId);
+    public static void throwAlreadyExistException(final String fileId) throws SisAlreadyException {
+        SisException.throwAlreadyException("EXAM SCHEDULE FILE IS ALREADY EXIST! fileId:" + fileId);
     }
 
-    public static void throwAlreadyDeletedException(final Long fileId) throws SisAlreadyException {
-        throwAlreadyException("EXAM SCHEDULE FILE IS ALREADY DELETED! fileId:" + fileId);
+    public static void throwFileTypeNotPdfException(final String fileType) throws SisFileTypeException {
+        SisException.throwFileTypeException("EXAM SCHEDULE FILE TYPE IS NOT PDF! fileType:" + fileType);
     }
 }
