bc1225d9c08475c8c22dc5dc03c9e15c9be7a404
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-17 20:18:24 +0300

    DepartmentInfoConverter and DepartmentEntity Classes Have Been Refactored

--- src/main/java/com/graduationproject/studentinformationsystem/university/department/model/entity/DepartmentEntity.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/department/model/entity/DepartmentEntity.java
@@ -2,9 +2,7 @@ package com.graduationproject.studentinformationsystem.university.department.mod
 
 import com.graduationproject.studentinformationsystem.common.model.entity.SisBaseEntity;
 import com.graduationproject.studentinformationsystem.university.department.model.enums.DepartmentStatus;
-import com.graduationproject.studentinformationsystem.university.faculty.model.entity.FacultyEntity;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
@@ -17,7 +15,4 @@ public class DepartmentEntity extends SisBaseEntity {
     private DepartmentStatus status;
     private Integer totalClassLevel;
     private Boolean isTherePreparatoryClass;
-
-    @Setter
-    private FacultyEntity facultyEntity;
 }
