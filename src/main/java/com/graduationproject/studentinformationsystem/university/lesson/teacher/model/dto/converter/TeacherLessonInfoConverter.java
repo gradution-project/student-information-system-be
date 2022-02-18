3f160282a1a4ae453a7c29ebe73c5f80739276d3
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-18 07:49:10 +0300

    TeacherLessonInfoConverter and TeacherLessonServiceImpl Classes Have Been Refactored

--- src/main/java/com/graduationproject/studentinformationsystem/university/lesson/teacher/model/dto/converter/TeacherLessonInfoConverter.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/lesson/teacher/model/dto/converter/TeacherLessonInfoConverter.java
@@ -2,7 +2,8 @@ package com.graduationproject.studentinformationsystem.university.lesson.teacher
 
 import com.graduationproject.studentinformationsystem.common.model.dto.request.SisOperationInfoRequest;
 import com.graduationproject.studentinformationsystem.common.util.SisUtil;
-import com.graduationproject.studentinformationsystem.university.lesson.common.model.dto.converter.LessonInfoConverter;
+import com.graduationproject.studentinformationsystem.university.lesson.common.model.dto.response.LessonResponse;
+import com.graduationproject.studentinformationsystem.university.lesson.common.service.LessonOutService;
 import com.graduationproject.studentinformationsystem.university.lesson.teacher.model.dto.request.TeacherLessonDeleteRequest;
 import com.graduationproject.studentinformationsystem.university.lesson.teacher.model.dto.request.TeacherLessonInfoRequest;
 import com.graduationproject.studentinformationsystem.university.lesson.teacher.model.dto.request.TeacherLessonSaveRequest;
@@ -10,18 +11,23 @@ import com.graduationproject.studentinformationsystem.university.lesson.teacher.
 import com.graduationproject.studentinformationsystem.university.lesson.teacher.model.entity.TeacherLessonDeleteEntity;
 import com.graduationproject.studentinformationsystem.university.lesson.teacher.model.entity.TeacherLessonEntity;
 import com.graduationproject.studentinformationsystem.university.lesson.teacher.model.entity.TeacherLessonSaveEntity;
-import com.graduationproject.studentinformationsystem.university.teacher.model.dto.converter.TeacherAcademicInfoConverter;
+import com.graduationproject.studentinformationsystem.university.teacher.model.dto.response.TeacherAcademicInfoResponse;
+import com.graduationproject.studentinformationsystem.university.teacher.service.TeacherOutService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class TeacherLessonInfoConverter {
 
-    private TeacherLessonInfoConverter() {
-    }
+    private final TeacherOutService teacherOutService;
+    private final LessonOutService lessonOutService;
 
-    public static TeacherLessonSaveEntity generateSaveEntity(final TeacherLessonSaveRequest saveRequest) {
+    public TeacherLessonSaveEntity generateSaveEntity(final TeacherLessonSaveRequest saveRequest) {
 
         final TeacherLessonInfoRequest teacherLessonInfoRequest = saveRequest.getTeacherLessonInfoRequest();
         final SisOperationInfoRequest operationInfoRequest = saveRequest.getOperationInfoRequest();
@@ -33,7 +39,7 @@ public class TeacherLessonInfoConverter {
                 .createdDate(new Date()).build();
     }
 
-    public static TeacherLessonDeleteEntity generateDeleteEntity(final TeacherLessonDeleteRequest deleteRequest) {
+    public TeacherLessonDeleteEntity generateDeleteEntity(final TeacherLessonDeleteRequest deleteRequest) {
 
         final TeacherLessonInfoRequest teacherLessonInfoRequest = deleteRequest.getTeacherLessonInfoRequest();
 
@@ -42,16 +48,19 @@ public class TeacherLessonInfoConverter {
                 .lessonId(teacherLessonInfoRequest.getLessonId()).build();
     }
 
-    public static TeacherLessonResponse entityToResponse(final TeacherLessonEntity teacherLessonEntity) {
+    public TeacherLessonResponse entityToResponse(final TeacherLessonEntity teacherLessonEntity) {
+
+        final TeacherAcademicInfoResponse teacherAcademicInfoResponse = teacherOutService.getTeacherAcademicInfoResponse(teacherLessonEntity.getTeacherId());
+        final LessonResponse lessonResponse = lessonOutService.getLessonResponse(teacherLessonEntity.getLessonId());
 
         return TeacherLessonResponse.builder()
                 .createdUserId(teacherLessonEntity.getCreatedUserId())
                 .createdDate(SisUtil.getFormattedDateTime(teacherLessonEntity.getCreatedDate()))
-                .teacherAcademicInfoResponse(TeacherAcademicInfoConverter.entityToResponse(teacherLessonEntity.getTeacherAcademicInfoEntity()))
-                .lessonResponse(LessonInfoConverter.entityToResponse(teacherLessonEntity.getLessonEntity())).build();
+                .lessonResponse(lessonResponse)
+                .teacherAcademicInfoResponse(teacherAcademicInfoResponse).build();
     }
 
-    public static List<TeacherLessonResponse> entitiesToResponses(final List<TeacherLessonEntity> lessonEntities) {
+    public List<TeacherLessonResponse> entitiesToResponses(final List<TeacherLessonEntity> lessonEntities) {
         final List<TeacherLessonResponse> lessonResponses = new ArrayList<>();
         lessonEntities.forEach(lessonEntity -> lessonResponses.add(entityToResponse(lessonEntity)));
         return lessonResponses;
