88d5f37330e2c6250bf3a6a5e3a778e3b0c75bb7
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-25 13:17:24 +0300

    Student Lesson Registration Request Classes Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/lesson/student/registration/model/dto/request/StudentLessonRegistrationInfoRequest.java
@@ -0,0 +1,23 @@
+package com.graduationproject.studentinformationsystem.university.lesson.student.registration.model.dto.request;
+
+import com.graduationproject.studentinformationsystem.common.util.validation.id.StudentID;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+public class StudentLessonRegistrationInfoRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -818997554323584139L;
+
+    @NotNull
+    @StudentID
+    private Long studentId;
+
+    @NotNull
+    private List<Long> lessonsIds;
+}
