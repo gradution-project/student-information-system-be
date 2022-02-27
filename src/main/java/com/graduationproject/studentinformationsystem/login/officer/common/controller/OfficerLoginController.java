195d0be5e67fb94724488e9864b7c9041e81faa6
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-27 21:48:10 +0300

    officerForgotPassword Method Has Been Deleted Because It's Unused

--- src/main/java/com/graduationproject/studentinformationsystem/login/officer/common/controller/OfficerLoginController.java
+++ src/main/java/com/graduationproject/studentinformationsystem/login/officer/common/controller/OfficerLoginController.java
@@ -1,13 +1,10 @@
 package com.graduationproject.studentinformationsystem.login.officer.common.controller;
 
 import com.graduationproject.studentinformationsystem.common.util.controller.response.SisBaseApiResponse;
-import com.graduationproject.studentinformationsystem.common.util.exception.SisNotExistException;
 import com.graduationproject.studentinformationsystem.login.common.controller.enpoint.LoginControllerEndpoint;
-import com.graduationproject.studentinformationsystem.login.common.model.response.ForgotPasswordResponse;
 import com.graduationproject.studentinformationsystem.login.common.model.response.LoginResponse;
-import com.graduationproject.studentinformationsystem.login.officer.model.dto.request.OfficerForgotPasswordRequest;
-import com.graduationproject.studentinformationsystem.login.officer.model.dto.request.OfficerLoginRequest;
-import com.graduationproject.studentinformationsystem.login.officer.service.OfficerLoginService;
+import com.graduationproject.studentinformationsystem.login.officer.common.model.dto.request.OfficerLoginRequest;
+import com.graduationproject.studentinformationsystem.login.officer.common.service.OfficerLoginService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -38,15 +35,5 @@ public class OfficerLoginController {
 
         final LoginResponse loginResponse = loginService.login(loginRequest);
         return successResponse(loginResponse);
-    }
-
-    @PostMapping(LoginControllerEndpoint.OFFICER_FORGOT_PASSWORD)
-    @ApiOperation(value = "Officer Forgot Password")
-    public ResponseEntity<SisBaseApiResponse<ForgotPasswordResponse>> officerForgotPassword(
-            @RequestBody @Valid final OfficerForgotPasswordRequest forgotPasswordRequest)
-            throws SisNotExistException {
-
-        final ForgotPasswordResponse forgotPasswordResponse = loginService.forgotPassword(forgotPasswordRequest);
-        return successResponse(forgotPasswordResponse);
     }
 }
