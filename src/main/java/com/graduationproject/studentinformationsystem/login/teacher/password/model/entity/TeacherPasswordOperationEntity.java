2fdc4970e3832db92862b74ac11fc80ff3eceba2
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-27 21:32:05 +0300

    TeacherPasswordOperation Entity Classes Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/login/teacher/password/model/entity/TeacherPasswordOperationEntity.java
@@ -0,0 +1,16 @@
+package com.graduationproject.studentinformationsystem.login.teacher.password.model.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class TeacherPasswordOperationEntity {
+
+    private String operationId;
+    private Long teacherId;
+    private LocalDateTime expireDate;
+    private String feUrl;
+}
