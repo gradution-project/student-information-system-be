9227863082e27bbea8059956eb95621298e0251f
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-27 19:54:25 +0300

    ChangePassword URL Has Been Added to Student Mails && Service Codes Has Been Refactored and Updated

--- src/main/java/com/graduationproject/studentinformationsystem/university/mail/service/StudentMailService.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/mail/service/StudentMailService.java
@@ -1,10 +1,11 @@
 package com.graduationproject.studentinformationsystem.university.mail.service;
 
+import com.graduationproject.studentinformationsystem.common.util.exception.SisNotExistException;
 import com.graduationproject.studentinformationsystem.university.student.model.dto.response.StudentInfoDetailResponse;
 
 public interface StudentMailService {
 
-    void sendFirstPasswordEmail(StudentInfoDetailResponse infoDetailResponse);
+    void sendSavedEmail(StudentInfoDetailResponse infoDetailResponse) throws SisNotExistException;
 
-    void sendForgotPasswordEmail(StudentInfoDetailResponse infoDetailResponse);
+    void sendForgotPasswordEmail(StudentInfoDetailResponse infoDetailResponse) throws SisNotExistException;
 }
