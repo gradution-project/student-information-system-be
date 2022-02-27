502068e41f0b088c7da5bc6d9573c19ba5be9d6c
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-27 19:49:44 +0300

    getStudentInfoDetailResponse Method Has Been Added to StudentOutService

--- src/main/java/com/graduationproject/studentinformationsystem/university/student/service/impl/StudentOutServiceImpl.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/student/service/impl/StudentOutServiceImpl.java
@@ -1,8 +1,10 @@
 package com.graduationproject.studentinformationsystem.university.student.service.impl;
 
 import com.graduationproject.studentinformationsystem.common.util.exception.SisNotExistException;
+import com.graduationproject.studentinformationsystem.university.student.model.dto.converter.StudentInfoResponseConverter;
 import com.graduationproject.studentinformationsystem.university.student.model.dto.converter.StudentResponseConverter;
 import com.graduationproject.studentinformationsystem.university.student.model.dto.response.StudentAcademicInfoResponse;
+import com.graduationproject.studentinformationsystem.university.student.model.dto.response.StudentInfoDetailResponse;
 import com.graduationproject.studentinformationsystem.university.student.model.dto.response.StudentInfoResponse;
 import com.graduationproject.studentinformationsystem.university.student.model.dto.response.StudentPersonalInfoResponse;
 import com.graduationproject.studentinformationsystem.university.student.model.exception.StudentException;
@@ -18,6 +20,13 @@ public class StudentOutServiceImpl implements StudentOutService {
 
     private final StudentAcademicInfoService academicInfoService;
     private final StudentPersonalInfoService personalInfoService;
+
+    @Override
+    public StudentInfoDetailResponse getStudentInfoDetailResponse(Long studentId) {
+        final StudentAcademicInfoResponse academicInfoResponse = academicInfoService.getStudentAcademicInfoById(studentId);
+        final StudentPersonalInfoResponse personalInfoResponse = personalInfoService.getStudentPersonalInfoById(studentId);
+        return StudentInfoResponseConverter.convert(academicInfoResponse, personalInfoResponse);
+    }
 
     @Override
     public StudentInfoResponse getStudentInfoResponse(final Long studentId) {
