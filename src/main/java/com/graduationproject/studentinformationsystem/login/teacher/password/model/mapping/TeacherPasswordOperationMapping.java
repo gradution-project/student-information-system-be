8ab4c346eedb1013a2713f1aeb55ec4b98088b98
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-27 21:31:03 +0300

    TeacherPasswordOperationMapping Class Has Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/login/teacher/password/model/mapping/TeacherPasswordOperationMapping.java
@@ -0,0 +1,24 @@
+package com.graduationproject.studentinformationsystem.login.teacher.password.model.mapping;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Getter
+@RequiredArgsConstructor
+public enum TeacherPasswordOperationMapping {
+
+    OPERATION_ID("OPERATION_ID", "operationId"),
+    TEACHER_ID("TEACHER_ID", "teacherId"),
+    EXPIRE_DATE("EXPIRE_DATE", "expireDate"),
+    FE_URL("FE_URL", "feUrl");
+
+    private final String columnName;
+    private final String modelName;
+
+    public static final Map<String, String> COLUMN_MAPPINGS = Stream.of(values())
+            .collect(Collectors.toMap(TeacherPasswordOperationMapping::getColumnName, TeacherPasswordOperationMapping::getModelName));
+}
