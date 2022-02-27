e07e184d087fd2ed4f97a51f72bfef70bc308d6a
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-27 17:53:09 +0300

    StudentPasswordForgotRequest Class Has Been Moved to password Package

--- src/main/java/com/graduationproject/studentinformationsystem/login/student/password/model/dto/request/StudentPasswordForgotRequest.java
+++ src/main/java/com/graduationproject/studentinformationsystem/login/student/password/model/dto/request/StudentPasswordForgotRequest.java
@@ -1,4 +1,4 @@
-package com.graduationproject.studentinformationsystem.login.student.model.dto.request;
+package com.graduationproject.studentinformationsystem.login.student.password.model.dto.request;
 
 import com.graduationproject.studentinformationsystem.common.util.validation.id.StudentID;
 import lombok.Getter;
@@ -8,7 +8,7 @@ import java.io.Serial;
 import java.io.Serializable;
 
 @Getter
-public class StudentForgotPasswordRequest implements Serializable {
+public class StudentPasswordForgotRequest implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -6073520178120747403L;
@@ -16,4 +16,7 @@ public class StudentForgotPasswordRequest implements Serializable {
     @NotNull
     @StudentID
     private Long studentId;
+
+    @NotNull
+    private String feUrl;
 }
