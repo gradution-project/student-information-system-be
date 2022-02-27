12e266cb10b682fb0b4c2b4526054410f92350dd
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-27 21:29:47 +0300

    getTeacherInfoDetailResponse and ifTeacherIsNotExistThrowNotExistException Methods Have Been Added to Teacher Out Service

--- src/main/java/com/graduationproject/studentinformationsystem/university/teacher/service/TeacherOutService.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/teacher/service/TeacherOutService.java
@@ -1,8 +1,14 @@
 package com.graduationproject.studentinformationsystem.university.teacher.service;
 
+import com.graduationproject.studentinformationsystem.common.util.exception.SisNotExistException;
+import com.graduationproject.studentinformationsystem.university.teacher.model.dto.response.TeacherInfoDetailResponse;
 import com.graduationproject.studentinformationsystem.university.teacher.model.dto.response.TeacherInfoResponse;
 
 public interface TeacherOutService {
 
+    TeacherInfoDetailResponse getTeacherInfoDetailResponse(Long teacherId) throws SisNotExistException;
+
     TeacherInfoResponse getTeacherInfoResponse(Long teacherId);
+
+    void ifTeacherIsNotExistThrowNotExistException(Long teacherId) throws SisNotExistException;
 }
