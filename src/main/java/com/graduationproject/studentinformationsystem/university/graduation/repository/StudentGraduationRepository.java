95f134871f16dac0c4226ac2fe355f9454a437af
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-03-25 15:02:07 +0300

    StudentGraduation Repository Classes Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/graduation/repository/StudentGraduationRepository.java
@@ -0,0 +1,31 @@
+package com.graduationproject.studentinformationsystem.university.graduation.repository;
+
+import com.graduationproject.studentinformationsystem.university.graduation.model.entity.StudentGraduationEntity;
+import com.graduationproject.studentinformationsystem.university.graduation.model.enums.StudentGraduationStatus;
+
+import java.util.List;
+
+public interface StudentGraduationRepository {
+
+    List<StudentGraduationEntity> getAllStudentsGraduationsByStatus(StudentGraduationStatus status);
+
+    StudentGraduationEntity getStudentGraduationDetailByGraduationId(String graduationId);
+
+    void saveStudentGraduation(StudentGraduationEntity graduationEntity);
+
+    void updateStudentGraduationStatus(StudentGraduationEntity graduationEntity);
+
+    boolean isStudentGraduationExist(String graduationId);
+
+    boolean isStudentGraduationWaiting(String graduationId);
+
+    boolean isStudentGraduationApproved(String graduationId);
+
+    boolean isStudentGraduationRejected(String graduationId);
+
+    boolean isStudentGraduationConfirmed(String graduationId);
+
+    boolean isStudentGraduationUnconfirmed(String graduationId);
+
+    String getGraduationId(Long studentId);
+}
