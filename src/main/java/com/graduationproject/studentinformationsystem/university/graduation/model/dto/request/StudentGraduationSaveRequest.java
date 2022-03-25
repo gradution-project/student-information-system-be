88ee571d8d91973dc9e78b08a6084f2968f571af
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-03-25 15:01:27 +0300

    StudentGraduation Request Classes Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/graduation/model/dto/request/StudentGraduationSaveRequest.java
@@ -0,0 +1,24 @@
+package com.graduationproject.studentinformationsystem.university.graduation.model.dto.request;
+
+import com.graduationproject.studentinformationsystem.common.model.dto.request.SisOperationInfoRequest;
+import com.graduationproject.studentinformationsystem.common.util.validation.id.StudentID;
+import lombok.Getter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+public class StudentGraduationSaveRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -482762006018299741L;
+
+    @NotNull
+    @StudentID
+    private Long studentId;
+
+    @Valid
+    private SisOperationInfoRequest operationInfoRequest;
+}
