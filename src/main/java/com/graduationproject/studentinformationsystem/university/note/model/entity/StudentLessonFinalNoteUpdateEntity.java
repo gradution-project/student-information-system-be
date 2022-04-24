e6122c626399dc6c914a70d3cedafd029fa01c68
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-04-24 09:23:58 +0300

    Student Lesson Note Entities Have Been Updated for Note States

--- src/main/java/com/graduationproject/studentinformationsystem/university/note/model/entity/StudentLessonFinalNoteUpdateEntity.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/note/model/entity/StudentLessonFinalNoteUpdateEntity.java
@@ -1,5 +1,6 @@
 package com.graduationproject.studentinformationsystem.university.note.model.entity;
 
+import com.graduationproject.studentinformationsystem.university.note.model.enums.StudentLessonNoteState;
 import com.graduationproject.studentinformationsystem.university.note.model.enums.StudentLessonNoteStatus;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +13,7 @@ public class StudentLessonFinalNoteUpdateEntity {
 
     private String id;
     private Double finalNote;
+    private StudentLessonNoteState finalNoteState;
     private Double meanOfNote;
     private StudentLessonNoteStatus status;
     private Long modifiedUserId;
