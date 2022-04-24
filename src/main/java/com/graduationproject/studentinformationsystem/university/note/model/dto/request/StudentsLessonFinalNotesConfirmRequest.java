b7170ec8a3f858c593418df4560a9902ba5ebf38
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-04-24 09:26:16 +0300

    Student Lesson Midterm/Final/Resit Note Confirm Request DTOs Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/note/model/dto/request/StudentsLessonFinalNotesConfirmRequest.java
@@ -0,0 +1,29 @@
+package com.graduationproject.studentinformationsystem.university.note.model.dto.request;
+
+import com.graduationproject.studentinformationsystem.common.model.dto.request.SisOperationInfoRequest;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+public class StudentsLessonFinalNotesConfirmRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -8612524900336578854L;
+
+    @NotNull
+    @NotEmpty
+    private List<String> lessonNoteIds;
+
+    @Valid
+    private SisOperationInfoRequest operationInfoRequest;
+}
