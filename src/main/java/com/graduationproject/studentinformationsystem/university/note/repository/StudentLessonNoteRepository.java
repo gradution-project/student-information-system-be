ee079683ca26dd4eeb8c470f8a74f6ba605654ab
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-04-24 09:25:42 +0300

    Confirm Student Lesson Midterm/Final/Resit Note Methods Have Been Added to StudentLessonNoteRepository & SQL Scripts Have Been Updated for MIDTERM_NOTE_STATE/FINAL_NOTE_STATE/RESIT_NOTE_STATE Fields

--- src/main/java/com/graduationproject/studentinformationsystem/university/note/repository/StudentLessonNoteRepository.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/note/repository/StudentLessonNoteRepository.java
@@ -16,9 +16,15 @@ public interface StudentLessonNoteRepository {
 
     void updateStudentLessonMidtermNote(StudentLessonMidtermNoteUpdateEntity updateEntity);
 
+    void confirmStudentLessonMidtermNote(StudentLessonMidtermNoteConfirmEntity confirmEntity);
+
     void updateStudentLessonFinalNote(StudentLessonFinalNoteUpdateEntity updateEntity);
 
+    void confirmStudentLessonFinalNote(StudentLessonFinalNoteConfirmEntity confirmEntity);
+
     void updateStudentLessonResitNote(StudentLessonResitNoteUpdateEntity updateEntity);
+
+    void confirmStudentLessonResitNote(StudentLessonResitNoteConfirmEntity confirmEntity);
 
     Double getMidtermNoteById(String id);
 
