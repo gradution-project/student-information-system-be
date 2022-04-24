d91b51951bd7cf575ca6abf52ad96cf48866ec1a
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-04-24 09:29:37 +0300

    Confirm Students Lesson Midterm/Final/Resit Notes Methods Have Been Added to StudentLessonNoteService

--- src/main/java/com/graduationproject/studentinformationsystem/university/note/service/StudentLessonNoteService.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/note/service/StudentLessonNoteService.java
@@ -1,9 +1,7 @@
 package com.graduationproject.studentinformationsystem.university.note.service;
 
 import com.graduationproject.studentinformationsystem.common.util.exception.SisNotExistException;
-import com.graduationproject.studentinformationsystem.university.note.model.dto.request.StudentsLessonFinalNotesUpdateRequest;
-import com.graduationproject.studentinformationsystem.university.note.model.dto.request.StudentsLessonMidtermNotesUpdateRequest;
-import com.graduationproject.studentinformationsystem.university.note.model.dto.request.StudentsLessonResitNotesUpdateRequest;
+import com.graduationproject.studentinformationsystem.university.note.model.dto.request.*;
 import com.graduationproject.studentinformationsystem.university.note.model.dto.response.StudentLessonNoteResponse;
 
 import java.util.List;
@@ -17,9 +15,18 @@ public interface StudentLessonNoteService {
     List<StudentLessonNoteResponse> updateStudentsLessonMidtermNotes(StudentsLessonMidtermNotesUpdateRequest updateRequest)
             throws SisNotExistException;
 
+    List<StudentLessonNoteResponse> confirmStudentsLessonMidtermNotes(StudentsLessonMidtermNotesConfirmRequest confirmRequest)
+            throws SisNotExistException;
+
     List<StudentLessonNoteResponse> updateStudentsLessonFinalNotes(StudentsLessonFinalNotesUpdateRequest updateRequest)
             throws SisNotExistException;
 
+    List<StudentLessonNoteResponse> confirmStudentsLessonFinalNotes(StudentsLessonFinalNotesConfirmRequest confirmRequest)
+            throws SisNotExistException;
+
     List<StudentLessonNoteResponse> updateStudentsLessonResitNotes(StudentsLessonResitNotesUpdateRequest updateRequest)
+            throws SisNotExistException;
+
+    List<StudentLessonNoteResponse> confirmStudentsLessonResitNotes(StudentsLessonResitNotesConfirmRequest confirmRequest)
             throws SisNotExistException;
 }
