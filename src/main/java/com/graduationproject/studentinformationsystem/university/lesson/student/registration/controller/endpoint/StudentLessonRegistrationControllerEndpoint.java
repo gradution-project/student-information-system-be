33c728a0bce660fce4f3c1f60279b8c62157e707
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-04-28 13:11:14 +0300

    getStudentLessonRegistrationDetailByStudentId Method Has Been Created in StudentLessonRegistrationController & GET Endpoints Have Been Updated

--- src/main/java/com/graduationproject/studentinformationsystem/university/lesson/student/registration/controller/endpoint/StudentLessonRegistrationControllerEndpoint.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/lesson/student/registration/controller/endpoint/StudentLessonRegistrationControllerEndpoint.java
@@ -5,13 +5,9 @@ public class StudentLessonRegistrationControllerEndpoint {
     private StudentLessonRegistrationControllerEndpoint() {
     }
 
-    private static final String REGISTRATION_ID = "/{registrationId}";
-    private static final String STUDENT_ID = "/{studentId}";
-    public static final String GET = "/get";
-    public static final String GET_BY_REGISTRATION_ID = GET + REGISTRATION_ID;
+    public static final String BY_REGISTRATION_ID = "/{registrationId}";
+    public static final String BY_STUDENT_ID = "/{studentId}";
     public static final String SAVE = "/save";
     public static final String APPROVE = "/approve";
     public static final String REJECT = "/reject";
-    public static final String WAITING_BY_STUDENT_ID = "/waiting" + STUDENT_ID;
-    public static final String APPROVED_BY_REGISTRATION_ID = "/approved" + REGISTRATION_ID;
 }
