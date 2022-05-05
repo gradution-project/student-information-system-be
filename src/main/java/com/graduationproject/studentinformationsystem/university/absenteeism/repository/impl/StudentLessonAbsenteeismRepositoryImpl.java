8dc27acbc66246039c8fdf92b762a398fb35d9d0
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-05-05 09:38:56 +0300

    StudentLessonAbsenteeism Repository Classes Have Been Created

---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/absenteeism/repository/impl/StudentLessonAbsenteeismRepositoryImpl.java
@@ -0,0 +1,299 @@
+package com.graduationproject.studentinformationsystem.university.absenteeism.repository.impl;
+
+import com.graduationproject.studentinformationsystem.common.util.exception.SisDatabaseException;
+import com.graduationproject.studentinformationsystem.common.util.log.SisErrorLogMessageUtil;
+import com.graduationproject.studentinformationsystem.common.util.log.SisInfoLogMessageUtil;
+import com.graduationproject.studentinformationsystem.common.util.log.SisWarnLogMessageUtil;
+import com.graduationproject.studentinformationsystem.university.absenteeism.model.entity.StudentLessonAbsenteeismEntity;
+import com.graduationproject.studentinformationsystem.university.absenteeism.model.entity.StudentLessonAbsenteeismSaveEntity;
+import com.graduationproject.studentinformationsystem.university.absenteeism.model.entity.StudentLessonAbsenteeismUpdateEntity;
+import com.graduationproject.studentinformationsystem.university.absenteeism.model.enums.StudentLessonAbsenteeismStatus;
+import com.graduationproject.studentinformationsystem.university.absenteeism.repository.StudentLessonAbsenteeismRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Connection;
+import org.sql2o.Query;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+import static com.graduationproject.studentinformationsystem.university.absenteeism.model.mapping.StudentLessonAbsenteeismMapping.*;
+import static com.graduationproject.studentinformationsystem.university.absenteeism.repository.impl.scripts.StudentLessonAbsenteeismSqlScripts.*;
+import static com.graduationproject.studentinformationsystem.university.note.model.mapping.StudentLessonNoteMapping.ID;
+
+@Repository
+@RequiredArgsConstructor
+public class StudentLessonAbsenteeismRepositoryImpl implements StudentLessonAbsenteeismRepository {
+
+    private static final String STUDENT_LESSON_ABSENTEEISM = "Student Lesson Absenteeism";
+    private final SisInfoLogMessageUtil info = SisInfoLogMessageUtil.builder().apiName(STUDENT_LESSON_ABSENTEEISM).build();
+    private final SisWarnLogMessageUtil warn = SisWarnLogMessageUtil.builder().apiName(STUDENT_LESSON_ABSENTEEISM).build();
+    private final SisErrorLogMessageUtil error = SisErrorLogMessageUtil.builder().apiName(STUDENT_LESSON_ABSENTEEISM).build();
+
+    private final Sql2o sql2o;
+
+    @Override
+    public List<StudentLessonAbsenteeismEntity> getAllStudentLessonsAbsenteeismByStudentId(final Long studentId,
+                                                                                           final Integer week) {
+
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(GET_ALL_STUDENT_LESSONS_ABSENTEEISM_BY_STUDENT_ID_AND_WEEK)) {
+
+            final List<StudentLessonAbsenteeismEntity> absenteeismEntities = query
+                    .addParameter(STUDENT_ID.getModelName(), studentId)
+                    .addParameter(WEEK.getModelName(), week)
+                    .setColumnMappings(COLUMN_MAPPINGS)
+                    .executeAndFetch(StudentLessonAbsenteeismEntity.class);
+
+            if (!absenteeismEntities.isEmpty()) {
+                info.foundAllByStudentId(studentId);
+            } else {
+                warn.notFoundAllByStudentId(studentId);
+            }
+            return absenteeismEntities;
+        } catch (Exception exception) {
+            error.errorWhenGettingAllByStudentId(studentId);
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public List<StudentLessonAbsenteeismEntity> getAllStudentsLessonsAbsenteeismByLessonId(final Long lessonId,
+                                                                                           final Integer week) {
+
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(GET_ALL_STUDENTS_LESSON_ABSENTEEISM_BY_LESSON_ID_AND_WEEK)) {
+
+            final List<StudentLessonAbsenteeismEntity> absenteeismEntities = query
+                    .addParameter(LESSON_ID.getModelName(), lessonId)
+                    .addParameter(WEEK.getModelName(), week)
+                    .setColumnMappings(COLUMN_MAPPINGS)
+                    .executeAndFetch(StudentLessonAbsenteeismEntity.class);
+
+            if (!absenteeismEntities.isEmpty()) {
+                info.foundAllByLessonId(lessonId);
+            } else {
+                warn.notFoundAllByLessonId(lessonId);
+            }
+            return absenteeismEntities;
+        } catch (Exception exception) {
+            error.errorWhenGettingAllByLessonId(lessonId);
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public StudentLessonAbsenteeismEntity getStudentLessonAbsenteeismById(final String id) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(GET_STUDENT_LESSON_ABSENTEEISM_BY_ID)) {
+
+            final StudentLessonAbsenteeismEntity absenteeismEntity = query
+                    .addParameter(ID.getModelName(), id)
+                    .setColumnMappings(COLUMN_MAPPINGS)
+                    .executeAndFetchFirst(StudentLessonAbsenteeismEntity.class);
+
+            if (absenteeismEntity != null) {
+                info.foundById(id);
+            } else {
+                warn.notFoundById(id);
+            }
+            return absenteeismEntity;
+        } catch (Exception exception) {
+            error.errorWhenGettingById(id);
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public void saveStudentLessonAbsenteeism(final StudentLessonAbsenteeismSaveEntity saveEntity) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(SAVE_STUDENT_LESSON_ABSENTEEISM)) {
+
+            query.addParameter(ID.getModelName(), saveEntity.getId())
+                    .addParameter(TEACHER_ID.getModelName(), saveEntity.getTeacherId())
+                    .addParameter(STUDENT_ID.getModelName(), saveEntity.getStudentId())
+                    .addParameter(LESSON_ID.getModelName(), saveEntity.getLessonId())
+                    .addParameter(WEEK.getModelName(), saveEntity.getWeek())
+                    .addParameter(MAX_THEORETICAL_HOURS.getModelName(), saveEntity.getMaxTheoreticalHours())
+                    .addParameter(MAX_PRACTICE_HOURS.getModelName(), saveEntity.getMaxPracticeHours())
+                    .addParameter(STATUS.getModelName(), saveEntity.getStatus())
+                    .addParameter(CREATED_DATE.getModelName(), saveEntity.getCreatedDate())
+                    .addParameter(CREATED_USER_ID.getModelName(), saveEntity.getCreatedUserId())
+                    .executeUpdate();
+
+            info.savedById(saveEntity.getId());
+        } catch (Exception exception) {
+            error.errorWhenSaving();
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public void updateStudentLessonAbsenteeism(final StudentLessonAbsenteeismUpdateEntity updateEntity) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(UPDATE_STUDENT_LESSON_ABSENTEEISM)) {
+
+            query.addParameter(ID.getModelName(), updateEntity.getId())
+                    .addParameter(THEORETICAL_HOURS.getModelName(), updateEntity.getTheoreticalHours())
+                    .addParameter(PRACTICE_HOURS.getModelName(), updateEntity.getPracticeHours())
+                    .addParameter(STATUS.getModelName(), updateEntity.getStatus())
+                    .addParameter(MODIFIED_DATE.getModelName(), updateEntity.getModifiedDate())
+                    .addParameter(MODIFIED_USER_ID.getModelName(), updateEntity.getModifiedUserId())
+                    .executeUpdate();
+
+            info.updated();
+        } catch (Exception exception) {
+            error.errorWhenUpdating();
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public void updateStudentLessonAbsenteeismStatus(String id, StudentLessonAbsenteeismStatus status) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(UPDATE_STUDENT_LESSON_ABSENTEEISM_STATUS)) {
+
+            query.addParameter(ID.getModelName(), id)
+                    .addParameter(STATUS.getModelName(), status)
+                    .executeUpdate();
+
+            info.updated();
+        } catch (Exception exception) {
+            error.errorWhenUpdating();
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public boolean isStudentLessonAbsenteeismExist(final String id) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(IS_STUDENT_LESSON_ABSENTEEISM_EXIST_BY_ID)) {
+
+            final boolean isStudentLessonAbsenteeismExist = query.addParameter(ID.getModelName(), id)
+                    .executeScalar(Boolean.class);
+
+            if (isStudentLessonAbsenteeismExist) {
+                info.foundById(id);
+                return true;
+            } else {
+                warn.notFoundById(id);
+                return false;
+            }
+        } catch (Exception exception) {
+            error.errorWhenGettingById(id);
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public Long getStudentIdByAbsenteeismId(final String id) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(GET_STUDENT_ID_BY_ID)) {
+
+            final Long studentId = query
+                    .addParameter(ID.getModelName(), id)
+                    .executeScalar(Long.class);
+
+            if (studentId != null) {
+                info.foundById(id);
+            } else {
+                warn.notFoundById(id);
+            }
+            return studentId;
+        } catch (Exception exception) {
+            error.errorWhenGettingById(id);
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public Long getLessonIdByAbsenteeismId(final String id) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(GET_LESSON_ID_BY_ID)) {
+
+            final Long lessonId = query
+                    .addParameter(ID.getModelName(), id)
+                    .executeScalar(Long.class);
+
+            if (lessonId != null) {
+                info.foundById(id);
+            } else {
+                warn.notFoundById(id);
+            }
+            return lessonId;
+        } catch (Exception exception) {
+            error.errorWhenGettingById(id);
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public boolean isStudentLessonAbsenteeismExist(final Long studentId) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(IS_STUDENT_LESSON_ABSENTEEISM_EXIST_BY_STUDENT_ID)) {
+
+            final boolean isStudentLessonAbsenteeismExist = query
+                    .addParameter(STUDENT_ID.getModelName(), studentId)
+                    .executeScalar(Boolean.class);
+
+            if (isStudentLessonAbsenteeismExist) {
+                info.foundById(studentId);
+                return true;
+            } else {
+                warn.notFoundById(studentId);
+                return false;
+            }
+        } catch (Exception exception) {
+            error.errorWhenGettingById(studentId);
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public Integer calculateTotalTheoreticalHours(final String id) {
+        return calculateTotalHours(id, CALCULATE_TOTAL_THEORETICAL_HOURS_BY_STUDENT_ID_AND_LESSON_ID);
+    }
+
+    @Override
+    public Integer calculateTotalPracticeHours(final String id) {
+        return calculateTotalHours(id, CALCULATE_TOTAL_PRACTICE_HOURS_BY_STUDENT_ID_AND_LESSON_ID);
+    }
+
+    private Integer calculateTotalHours(final String id, final String sql) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(sql)) {
+
+            final Integer totalHours = query
+                    .addParameter(ID.getModelName(), id)
+                    .executeScalar(Integer.class);
+
+            if (totalHours != null) {
+                info.foundById(id);
+            } else {
+                warn.notFoundById(id);
+            }
+            return totalHours;
+        } catch (Exception exception) {
+            error.errorWhenGettingById(id);
+            throw new SisDatabaseException(exception);
+        }
+    }
+
+    @Override
+    public Integer getMaxTheoreticalHoursById(final String id) {
+        return getMaxHours(id, GET_MAX_THEORETICAL_HOURS_BY_STUDENT_ID_AND_LESSON_ID);
+    }
+
+    @Override
+    public Integer getMaxPracticeHoursById(final String id) {
+        return getMaxHours(id, GET_MAX_PRACTICE_HOURS_BY_STUDENT_ID_AND_LESSON_ID);
+    }
+
+    private Integer getMaxHours(final String id, final String sql) {
+        try (final Connection connection = sql2o.open(); final Query query = connection.createQuery(sql)) {
+
+            final Integer maxHours = query
+                    .addParameter(ID.getModelName(), id)
+                    .executeScalar(Integer.class);
+
+            if (maxHours != null) {
+                info.foundById(id);
+            } else {
+                warn.notFoundById(id);
+            }
+            return maxHours;
+        } catch (Exception exception) {
+            error.errorWhenGettingById(id);
+            throw new SisDatabaseException(exception);
+        }
+    }
+}
---
+++ src/main/java/com/graduationproject/studentinformationsystem/university/absenteeism/repository/impl/scripts/StudentLessonAbsenteeismSqlScripts.java
@@ -0,0 +1,177 @@
+package com.graduationproject.studentinformationsystem.university.absenteeism.repository.impl.scripts;
+
+import com.graduationproject.studentinformationsystem.common.util.sql.SisSqlUtil;
+
+import static com.graduationproject.studentinformationsystem.university.absenteeism.model.mapping.StudentLessonAbsenteeismMapping.ID;
+import static com.graduationproject.studentinformationsystem.university.absenteeism.model.mapping.StudentLessonAbsenteeismMapping.STUDENT_ID;
+
+public class StudentLessonAbsenteeismSqlScripts {
+
+    private static final StringBuilder sqlBuilder = new StringBuilder();
+
+    private StudentLessonAbsenteeismSqlScripts() {
+    }
+
+    /**
+     * SELECT ID, TEACHER_ID, STUDENT_ID, LESSON_ID, WEEK, THEORETICAL_HOURS, PRACTICE_HOURS, STATUS,
+     * CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID FROM STUDENT_LESSON_ABSENTEEISM;
+     */
+    private static final String GET_ALL_STUDENTS_LESSONS_ABSENTEEISM =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("SELECT ID, TEACHER_ID, STUDENT_ID, LESSON_ID, WEEK, THEORETICAL_HOURS, PRACTICE_HOURS, " +
+                            "STATUS, CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID " +
+                            "FROM STUDENT_LESSON_ABSENTEEISM ").toString();
+
+    /**
+     * SELECT ID, TEACHER_ID, STUDENT_ID, LESSON_ID, WEEK, THEORETICAL_HOURS, PRACTICE_HOURS, STATUS,
+     * CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID
+     * FROM STUDENT_LESSON_ABSENTEEISM WHERE STUDENT_ID=:studentId AND WEEK=:week;
+     */
+    public static final String GET_ALL_STUDENT_LESSONS_ABSENTEEISM_BY_STUDENT_ID_AND_WEEK =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append(GET_ALL_STUDENTS_LESSONS_ABSENTEEISM)
+                    .append("WHERE STUDENT_ID=:studentId AND WEEK=:week").toString();
+
+    /**
+     * SELECT ID, TEACHER_ID, STUDENT_ID, LESSON_ID, WEEK, THEORETICAL_HOURS, PRACTICE_HOURS, STATUS,
+     * CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID
+     * FROM STUDENT_LESSON_ABSENTEEISM WHERE LESSON_ID=:lessonId AND WEEK=:week;
+     */
+    public static final String GET_ALL_STUDENTS_LESSON_ABSENTEEISM_BY_LESSON_ID_AND_WEEK =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append(GET_ALL_STUDENTS_LESSONS_ABSENTEEISM)
+                    .append("WHERE LESSON_ID=:lessonId AND WEEK=:week").toString();
+
+    /**
+     * SELECT ID, TEACHER_ID, STUDENT_ID, LESSON_ID, THEORETICAL_HOURS, PRACTICE_HOURS, STATUS,
+     * CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID
+     * FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id;
+     */
+    public static final String GET_STUDENT_LESSON_ABSENTEEISM_BY_ID =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append(GET_ALL_STUDENTS_LESSONS_ABSENTEEISM)
+                    .append("WHERE ID=:id").toString();
+
+    /**
+     * INSERT INTO STUDENT_LESSON_ABSENTEEISM (ID, TEACHER_ID, STUDENT_ID, LESSON_ID, WEEK,
+     * MAX_THEORETICAL_HOURS, MAX_PRACTICE_HOURS, STATUS, CREATED_DATE, CREATED_USER_ID)
+     * VALUES (:id, :teacherId, :studentId, :lessonId, :week, :maxTheoreticalHours, :maxPracticeHours,
+     * :status, :createdDate, :createdUserId);
+     */
+    public static final String SAVE_STUDENT_LESSON_ABSENTEEISM =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("INSERT INTO STUDENT_LESSON_ABSENTEEISM (ID, TEACHER_ID, STUDENT_ID, LESSON_ID, WEEK, " +
+                            "MAX_THEORETICAL_HOURS, MAX_PRACTICE_HOURS, STATUS, CREATED_DATE, CREATED_USER_ID) " +
+                            "VALUES (:id, :teacherId,:studentId, :lessonId, :week, " +
+                            ":maxTheoreticalHours, :maxPracticeHours, :status, :createdDate, :createdUserId)").toString();
+
+    /**
+     * UPDATE STUDENT_LESSON_ABSENTEEISM SET THEORETICAL_HOURS=:theoreticalHours,
+     * PRACTICE_HOURS=:practiceHours, STATUS=:status, MODIFIED_DATE=:modifiedDate, MODIFIED_USER_ID=:modifiedUserId
+     * WHERE ID=:id;
+     */
+    public static final String UPDATE_STUDENT_LESSON_ABSENTEEISM =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("UPDATE STUDENT_LESSON_ABSENTEEISM SET THEORETICAL_HOURS=:theoreticalHours, " +
+                            "PRACTICE_HOURS=:practiceHours, STATUS=:status, MODIFIED_DATE=:modifiedDate, " +
+                            "MODIFIED_USER_ID=:modifiedUserId WHERE ID=:id").toString();
+
+    /**
+     * UPDATE STUDENT_LESSON_ABSENTEEISM SET STATUS=:status WHERE ID=:id;
+     */
+    public static final String UPDATE_STUDENT_LESSON_ABSENTEEISM_STATUS =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("UPDATE STUDENT_LESSON_ABSENTEEISM SET STATUS=:status WHERE ID=:id").toString();
+
+    /**
+     * SELECT CASE WHEN MAX(ID) IS NULL THEN 'false' ELSE 'true' END IS_EXIST
+     * FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id;
+     */
+    public static final String IS_STUDENT_LESSON_ABSENTEEISM_EXIST_BY_ID = SisSqlUtil
+            .isExistByColumnName("STUDENT_LESSON_ABSENTEEISM",
+                    ID.getColumnName(),
+                    ID.getModelName());
+
+    /**
+     * SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id;
+     */
+    public static final String GET_STUDENT_ID_BY_ID =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id").toString();
+
+    /**
+     * SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id;
+     */
+    public static final String GET_LESSON_ID_BY_ID =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id").toString();
+
+    /**
+     * SELECT CASE WHEN MAX(ID) IS NULL THEN 'false' ELSE 'true' END IS_EXIST
+     * FROM STUDENT_LESSON_ABSENTEEISM WHERE STUDENT_ID=:studentId;
+     */
+    public static final String IS_STUDENT_LESSON_ABSENTEEISM_EXIST_BY_STUDENT_ID = SisSqlUtil
+            .isExistByColumnName("STUDENT_LESSON_ABSENTEEISM",
+                    STUDENT_ID.getColumnName(),
+                    STUDENT_ID.getModelName());
+
+    /**
+     * SELECT SUM(THEORETICAL_HOURS) AS TOTAL_THEORETICAL_HOURS FROM STUDENT_LESSON_ABSENTEEISM
+     * WHERE
+     * STUDENT_ID=(SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id)
+     * AND
+     * LESSON_ID=(SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id);
+     */
+    public static final String CALCULATE_TOTAL_THEORETICAL_HOURS_BY_STUDENT_ID_AND_LESSON_ID =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("SELECT SUM(THEORETICAL_HOURS) AS TOTAL_THEORETICAL_HOURS FROM STUDENT_LESSON_ABSENTEEISM " +
+                            "WHERE " +
+                            "STUDENT_ID=(SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id) " +
+                            "AND " +
+                            "LESSON_ID=(SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id)").toString();
+
+    /**
+     * SELECT SUM(PRACTICE_HOURS) AS TOTAL_PRACTICE_HOURS FROM STUDENT_LESSON_ABSENTEEISM
+     * WHERE
+     * STUDENT_ID=(SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id)
+     * AND
+     * LESSON_ID=(SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id);
+     */
+    public static final String CALCULATE_TOTAL_PRACTICE_HOURS_BY_STUDENT_ID_AND_LESSON_ID =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("SELECT SUM(PRACTICE_HOURS) AS TOTAL_PRACTICE_HOURS FROM STUDENT_LESSON_ABSENTEEISM " +
+                            "WHERE " +
+                            "STUDENT_ID=(SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id) " +
+                            "AND " +
+                            "LESSON_ID=(SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id)").toString();
+
+    /**
+     * SELECT MAX_THEORETICAL_HOURS FROM STUDENT_LESSON_ABSENTEEISM
+     * WHERE
+     * STUDENT_ID=(SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id)
+     * AND
+     * LESSON_ID=(SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id);
+     */
+    public static final String GET_MAX_THEORETICAL_HOURS_BY_STUDENT_ID_AND_LESSON_ID =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("SELECT MAX_THEORETICAL_HOURS FROM STUDENT_LESSON_ABSENTEEISM " +
+                            "WHERE " +
+                            "STUDENT_ID=(SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id) " +
+                            "AND " +
+                            "LESSON_ID=(SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id)").toString();
+
+    /**
+     * SELECT MAX_PRACTICE_HOURS FROM STUDENT_LESSON_ABSENTEEISM
+     * WHERE
+     * STUDENT_ID=(SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id)
+     * AND
+     * LESSON_ID=(SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id);
+     */
+    public static final String GET_MAX_PRACTICE_HOURS_BY_STUDENT_ID_AND_LESSON_ID =
+            sqlBuilder.delete(0, sqlBuilder.length())
+                    .append("SELECT MAX_PRACTICE_HOURS FROM STUDENT_LESSON_ABSENTEEISM " +
+                            "WHERE " +
+                            "STUDENT_ID=(SELECT STUDENT_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id) " +
+                            "AND " +
+                            "LESSON_ID=(SELECT LESSON_ID FROM STUDENT_LESSON_ABSENTEEISM WHERE ID=:id)").toString();
+}
