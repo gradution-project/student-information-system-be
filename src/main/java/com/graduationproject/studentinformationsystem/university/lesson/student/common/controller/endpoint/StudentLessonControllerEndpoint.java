50e2573ee3de8def0e83e42f90488ceb23950c99
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-05-06 21:26:11 +0300

    StudentLessonController All Endpoints Have Been Fixed and Updated

--- src/main/java/com/graduationproject/studentinformationsystem/university/lesson/student/common/controller/endpoint/StudentLessonControllerEndpoint.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/lesson/student/common/controller/endpoint/StudentLessonControllerEndpoint.java
@@ -1,11 +1,14 @@
 package com.graduationproject.studentinformationsystem.university.lesson.student.common.controller.endpoint;
 
+import com.graduationproject.studentinformationsystem.common.util.controller.endpoint.SisControllerEndpoint;
+
 public class StudentLessonControllerEndpoint {
 
     private StudentLessonControllerEndpoint() {
     }
 
-    private static final String STUDENT_ID = "/{studentId}";
-    public static final String GET_BY_STUDENT_ID = "/get" + STUDENT_ID;
-    public static final String DELETE_BY_STUDENT_ID = "/delete" + STUDENT_ID;
+    public static final String ALL = SisControllerEndpoint.Path.STUDENT_LESSONS;
+    public static final String BASE = SisControllerEndpoint.Path.STUDENT_LESSON;
+    public static final String BY_STUDENT_ID = BASE + SisControllerEndpoint.Path.By.BY_STUDENT_ID;
+    public static final String ALL_BY_STUDENT_ID = ALL + SisControllerEndpoint.Path.By.BY_STUDENT_ID;
 }
