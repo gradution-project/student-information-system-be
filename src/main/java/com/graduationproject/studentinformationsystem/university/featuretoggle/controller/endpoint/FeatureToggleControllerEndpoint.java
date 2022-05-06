0f42748a1a38988d29551bbc073d99df1c873843
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-05-06 21:19:28 +0300

    FeatureToggleController All Endpoints Have Been Fixed and Updated

--- src/main/java/com/graduationproject/studentinformationsystem/university/featuretoggle/controller/endpoint/FeatureToggleControllerEndpoint.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/featuretoggle/controller/endpoint/FeatureToggleControllerEndpoint.java
@@ -1,12 +1,16 @@
 package com.graduationproject.studentinformationsystem.university.featuretoggle.controller.endpoint;
 
+import com.graduationproject.studentinformationsystem.common.util.controller.endpoint.SisControllerEndpoint;
+
 public class FeatureToggleControllerEndpoint {
 
     private FeatureToggleControllerEndpoint() {
     }
 
-    public static final String NAME = "/{name}";
-    public static final String ENABLE = "/enable";
-    public static final String DISABLE = "/disable";
-    public static final String ENABLED_BY_NAME = "/enabled" + NAME;
+    public static final String ALL = SisControllerEndpoint.Path.FEATURE_TOGGLES;
+    public static final String BASE = SisControllerEndpoint.Path.FEATURE_TOGGLE;
+    public static final String BY_NAME = BASE + "/{name}";
+    public static final String ENABLE = BASE + "/enable";
+    public static final String DISABLE = BASE + "/disable";
+    public static final String ENABLED_BY_NAME = BASE + "/enabled/{name}";
 }
