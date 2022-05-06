df2554edc8c1995af73fb367fac1013545047829
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-05-06 21:29:21 +0300

    TeacherPasswordOperationController All Endpoints Have Been Fixed and Updated

--- src/main/java/com/graduationproject/studentinformationsystem/login/teacher/password/controller/TeacherPasswordOperationController.java
+++ src/main/java/com/graduationproject/studentinformationsystem/login/teacher/password/controller/TeacherPasswordOperationController.java
@@ -18,22 +18,21 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 import static com.graduationproject.studentinformationsystem.common.config.SisSwaggerConfiguration.TEACHER_PASSWORD_OPERATION_API_TAG;
-import static com.graduationproject.studentinformationsystem.common.util.controller.endpoint.SisControllerEndpoint.Path.TEACHER_PASSWORD_OPERATION;
 import static com.graduationproject.studentinformationsystem.common.util.controller.response.SisResponseUtil.successResponse;
 import static com.graduationproject.studentinformationsystem.common.util.controller.response.SisResponseUtil.unauthorizedResponse;
 
 @RestController
-@RequestMapping(TEACHER_PASSWORD_OPERATION)
+@RequestMapping
 @Api(tags = TEACHER_PASSWORD_OPERATION_API_TAG)
 @RequiredArgsConstructor
 public class TeacherPasswordOperationController {
 
     private final TeacherPasswordOperationService passwordOperationService;
 
-    @GetMapping
-    @ApiOperation(value = "Is Teacher Password Change Operation Enabled")
+    @GetMapping(TeacherPasswordOperationControllerEndpoint.ENABLED)
+    @ApiOperation(value = "Is Teacher Password Change Operation Enabled Operation ID")
     public ResponseEntity<SisApiResponse> isPasswordChangeOperationEnabled(
-            @RequestParam final String operationId) throws SisNotExistException {
+            @PathVariable final String operationId) throws SisNotExistException {
 
         final boolean isPasswordChangeEnabled = passwordOperationService.isPasswordChangeEnabled(operationId);
 
@@ -44,7 +43,7 @@ public class TeacherPasswordOperationController {
         }
     }
 
-    @PostMapping(TeacherPasswordOperationControllerEndpoint.CHANGE_PASSWORD)
+    @PostMapping(TeacherPasswordOperationControllerEndpoint.CHANGE)
     @ApiOperation(value = "Change Teacher Password")
     public ResponseEntity<SisBaseApiResponse<TeacherPasswordChangeResponse>> changePassword(
             @RequestBody final TeacherPasswordChangeRequest passwordChangeRequest) throws SisNotExistException {
@@ -53,7 +52,7 @@ public class TeacherPasswordOperationController {
         return successResponse(passwordChangeResponse);
     }
 
-    @PostMapping(TeacherPasswordOperationControllerEndpoint.FORGOT_PASSWORD)
+    @PostMapping(TeacherPasswordOperationControllerEndpoint.FORGOT)
     @ApiOperation(value = "Teacher Forgot Password")
     public ResponseEntity<SisBaseApiResponse<TeacherPasswordForgotResponse>> forgotPassword(
             @RequestBody @Valid final TeacherPasswordForgotRequest passwordForgotRequest)
