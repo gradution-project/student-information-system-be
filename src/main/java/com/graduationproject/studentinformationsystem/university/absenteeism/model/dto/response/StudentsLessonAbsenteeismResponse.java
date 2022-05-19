6e9fea491d4b9814f0b651b452bf46bd1d7c90ad
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-05-19 13:56:09 +0300

    theoreticalHoursState and practiceHoursState Have Been Added to StudentsLessonAbsenteeismResponse Class

--- src/main/java/com/graduationproject/studentinformationsystem/university/absenteeism/model/dto/response/StudentsLessonAbsenteeismResponse.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/absenteeism/model/dto/response/StudentsLessonAbsenteeismResponse.java
@@ -1,6 +1,7 @@
 package com.graduationproject.studentinformationsystem.university.absenteeism.model.dto.response;
 
 import com.graduationproject.studentinformationsystem.common.model.dto.response.SisBaseResponse;
+import com.graduationproject.studentinformationsystem.university.absenteeism.model.enums.StudentLessonAbsenteeismHoursState;
 import com.graduationproject.studentinformationsystem.university.absenteeism.model.enums.StudentLessonAbsenteeismStatus;
 import com.graduationproject.studentinformationsystem.university.lesson.common.model.dto.response.LessonResponse;
 import com.graduationproject.studentinformationsystem.university.student.model.dto.response.StudentInfoResponse;
@@ -14,7 +15,9 @@ public class StudentsLessonAbsenteeismResponse extends SisBaseResponse {
 
     private String id;
     private Integer week;
+    private StudentLessonAbsenteeismHoursState theoreticalHoursState;
     private Integer theoreticalHours;
+    private StudentLessonAbsenteeismHoursState practiceHoursState;
     private Integer practiceHours;
     private StudentLessonAbsenteeismStatus status;
 
