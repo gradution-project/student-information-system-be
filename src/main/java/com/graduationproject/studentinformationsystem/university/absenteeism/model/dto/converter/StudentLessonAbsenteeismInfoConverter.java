99719cc72695e991baaf1b468fd99da8070a0c22
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-05-19 13:56:42 +0300

    theoreticalHoursState and practiceHoursState Fields Have Been Added to Converter Methods

--- src/main/java/com/graduationproject/studentinformationsystem/university/absenteeism/model/dto/converter/StudentLessonAbsenteeismInfoConverter.java
+++ src/main/java/com/graduationproject/studentinformationsystem/university/absenteeism/model/dto/converter/StudentLessonAbsenteeismInfoConverter.java
@@ -3,10 +3,13 @@ package com.graduationproject.studentinformationsystem.university.absenteeism.mo
 import com.graduationproject.studentinformationsystem.common.model.dto.request.SisOperationInfoRequest;
 import com.graduationproject.studentinformationsystem.common.util.SisUtil;
 import com.graduationproject.studentinformationsystem.university.absenteeism.model.dto.request.StudentLessonAbsenteeismSaveRequest;
+import com.graduationproject.studentinformationsystem.university.absenteeism.model.dto.response.StudentLessonsAbsenteeismResponse;
 import com.graduationproject.studentinformationsystem.university.absenteeism.model.dto.response.StudentsLessonAbsenteeismResponse;
-import com.graduationproject.studentinformationsystem.university.absenteeism.model.entity.StudentLessonAbsenteeismEntity;
 import com.graduationproject.studentinformationsystem.university.absenteeism.model.entity.StudentLessonAbsenteeismSaveEntity;
 import com.graduationproject.studentinformationsystem.university.absenteeism.model.entity.StudentLessonAbsenteeismUpdateEntity;
+import com.graduationproject.studentinformationsystem.university.absenteeism.model.entity.StudentLessonsAbsenteeismEntity;
+import com.graduationproject.studentinformationsystem.university.absenteeism.model.entity.StudentsLessonAbsenteeismEntity;
+import com.graduationproject.studentinformationsystem.university.absenteeism.model.enums.StudentLessonAbsenteeismHoursState;
 import com.graduationproject.studentinformationsystem.university.absenteeism.model.enums.StudentLessonAbsenteeismStatus;
 import com.graduationproject.studentinformationsystem.university.lesson.common.model.dto.response.LessonResponse;
 import com.graduationproject.studentinformationsystem.university.lesson.common.service.LessonOutService;
@@ -33,13 +36,29 @@ public class StudentLessonAbsenteeismInfoConverter {
 
         final SisOperationInfoRequest operationInfoRequest = saveRequest.getOperationInfoRequest();
 
+        final StudentLessonAbsenteeismHoursState theoreticalHoursState;
+        if (saveRequest.getMaxTheoreticalHours() == 0) {
+            theoreticalHoursState = StudentLessonAbsenteeismHoursState.NOT_EXIST;
+        } else {
+            theoreticalHoursState = StudentLessonAbsenteeismHoursState.NOT_ENTERED;
+        }
+
+        final StudentLessonAbsenteeismHoursState practiceHoursState;
+        if (saveRequest.getMaxPracticeHours() == 0) {
+            practiceHoursState = StudentLessonAbsenteeismHoursState.NOT_EXIST;
+        } else {
+            practiceHoursState = StudentLessonAbsenteeismHoursState.NOT_ENTERED;
+        }
+
         return StudentLessonAbsenteeismSaveEntity.builder()
                 .id(SisUtil.generateRandomUUID())
                 .teacherId(saveRequest.getTeacherId())
                 .studentId(saveRequest.getStudentId())
                 .lessonId(saveRequest.getLessonId())
                 .week(saveRequest.getWeek())
+                .theoreticalHoursState(theoreticalHoursState)
                 .maxTheoreticalHours(saveRequest.getMaxTheoreticalHours())
+                .practiceHoursState(practiceHoursState)
                 .maxPracticeHours(saveRequest.getMaxPracticeHours())
                 .status(StudentLessonAbsenteeismStatus.NOT_ENTERED)
                 .createdUserId(operationInfoRequest.getUserId())
@@ -52,9 +71,21 @@ public class StudentLessonAbsenteeismInfoConverter {
                                                                      final Integer practiceHours,
                                                                      final SisOperationInfoRequest operationInfoRequest) {
 
+        StudentLessonAbsenteeismHoursState theoreticalHoursState = StudentLessonAbsenteeismHoursState.NOT_ENTERED;
+        if (theoreticalHours != null) {
+            theoreticalHoursState = StudentLessonAbsenteeismHoursState.ENTERED;
+        }
+
+        StudentLessonAbsenteeismHoursState practiceHoursState = StudentLessonAbsenteeismHoursState.NOT_ENTERED;
+        if (practiceHours != null) {
+            practiceHoursState = StudentLessonAbsenteeismHoursState.ENTERED;
+        }
+
         return StudentLessonAbsenteeismUpdateEntity.builder()
                 .id(absenteeismId)
+                .theoreticalHoursState(theoreticalHoursState)
                 .theoreticalHours(theoreticalHours)
+                .practiceHoursState(practiceHoursState)
                 .practiceHours(practiceHours)
                 .status(StudentLessonAbsenteeismStatus.ENTERED)
                 .modifiedUserId(operationInfoRequest.getUserId())
@@ -62,7 +93,40 @@ public class StudentLessonAbsenteeismInfoConverter {
                 .build();
     }
 
-    public StudentsLessonAbsenteeismResponse entityToResponse(final StudentLessonAbsenteeismEntity absenteeismEntity) {
+    public StudentLessonsAbsenteeismResponse entityToResponse(final Integer currentTheoreticalHours,
+                                                              final Integer currentPracticeHours,
+                                                              final Integer balanceTheoreticalHours,
+                                                              final Integer balancePracticeHours,
+                                                              final StudentLessonsAbsenteeismEntity absenteeismEntity) {
+
+        final Long teacherId = absenteeismEntity.getTeacherId();
+        final TeacherInfoResponse teacherResponse = teacherOutService.getTeacherInfoResponse(teacherId);
+
+        final Long studentId = absenteeismEntity.getStudentId();
+        final StudentInfoResponse studentResponse = studentOutService.getStudentInfoResponse(studentId);
+
+        final Long lessonId = absenteeismEntity.getLessonId();
+        final LessonResponse lessonResponse = lessonOutService.getLessonResponse(lessonId);
+
+        return StudentLessonsAbsenteeismResponse.builder()
+                .id(absenteeismEntity.getId())
+                .theoreticalHoursState(absenteeismEntity.getTheoreticalHoursState())
+                .currentTheoreticalHours(currentTheoreticalHours)
+                .balanceTheoreticalHours(balanceTheoreticalHours)
+                .practiceHoursState(absenteeismEntity.getPracticeHoursState())
+                .currentPracticeHours(currentPracticeHours)
+                .balancePracticeHours(balancePracticeHours)
+                .status(absenteeismEntity.getStatus())
+                .createdDate(SisUtil.getFormattedDateTime(absenteeismEntity.getCreatedDate()))
+                .createdUserId(absenteeismEntity.getCreatedUserId())
+                .modifiedDate(SisUtil.getFormattedDateTime(absenteeismEntity.getModifiedDate()))
+                .modifiedUserId(absenteeismEntity.getModifiedUserId())
+                .teacherResponse(teacherResponse)
+                .studentResponse(studentResponse)
+                .lessonResponse(lessonResponse).build();
+    }
+
+    public StudentsLessonAbsenteeismResponse entityToResponse(final StudentsLessonAbsenteeismEntity absenteeismEntity) {
 
         final Long teacherId = absenteeismEntity.getTeacherId();
         final TeacherInfoResponse teacherResponse = teacherOutService.getTeacherInfoResponse(teacherId);
@@ -76,7 +140,9 @@ public class StudentLessonAbsenteeismInfoConverter {
         return StudentsLessonAbsenteeismResponse.builder()
                 .id(absenteeismEntity.getId())
                 .week(absenteeismEntity.getWeek())
+                .theoreticalHoursState(absenteeismEntity.getTheoreticalHoursState())
                 .theoreticalHours(absenteeismEntity.getTheoreticalHours())
+                .practiceHoursState(absenteeismEntity.getPracticeHoursState())
                 .practiceHours(absenteeismEntity.getPracticeHours())
                 .status(absenteeismEntity.getStatus())
                 .createdDate(SisUtil.getFormattedDateTime(absenteeismEntity.getCreatedDate()))
@@ -88,7 +154,7 @@ public class StudentLessonAbsenteeismInfoConverter {
                 .lessonResponse(lessonResponse).build();
     }
 
-    public List<StudentsLessonAbsenteeismResponse> entitiesToResponses(final List<StudentLessonAbsenteeismEntity> absenteeismEntities) {
+    public List<StudentsLessonAbsenteeismResponse> entitiesToResponses(final List<StudentsLessonAbsenteeismEntity> absenteeismEntities) {
         List<StudentsLessonAbsenteeismResponse> absenteeismResponses = new ArrayList<>();
         absenteeismEntities.forEach(absenteeismEntity -> absenteeismResponses.add(entityToResponse(absenteeismEntity)));
         return absenteeismResponses;
